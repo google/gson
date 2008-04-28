69360e508b83a54d101b0aa37dd5e4c5e721d32d
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-04-28 16:35:03 +0000

    Added a package-info file to place package-level comments.

---
+++ trunk/src/main/java/com/google/gson/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package provides a Java library for converting JSON to Java objects and
+ * vice-versa.  
+ * <p>
+ * The primary class to use is {@link Gson} which can be constructed with 
+ * {@code new Gson()} (using default settings) or by using {@link GsonBuilder} 
+ * (to configure various options such as using versioning and so on).
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.google.gson;
