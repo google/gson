b74e918e3d74854117be160d4f6a1cd3415734a2
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-04-29 18:22:35 +0000

    small test for compact formatter.

---
+++ trunk/src/test/java/com/google/gson/JsonCompactFormatterTest.java
@@ -0,0 +1,41 @@
+package com.google.gson;
+
+import com.google.gson.common.TestTypes.BagOfPrimitives;
+import com.google.gson.common.TestTypes.ClassWithTransientFields;
+import com.google.gson.common.TestTypes.Nested;
+import com.google.gson.common.TestTypes.PrimitiveArray;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonCompactFormatterTest extends TestCase {
+
+  private Gson gson;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    JsonFormatter formatter = new JsonCompactFormatter();
+    gson = new GsonBuilder().setFormatter(formatter).create();
+  }
+
+  @SuppressWarnings("unchecked")
+  public void testNoWhiteSpace() {
+    List list = new ArrayList();
+    list.add(new BagOfPrimitives());
+    list.add(new Nested());
+    list.add(new PrimitiveArray());
+    list.add(new ClassWithTransientFields());
+
+    String json = gson.toJson(list);
+    assertContainsNoWhiteSpace(json);
+  }
+
+  private void assertContainsNoWhiteSpace(String str) {
+    for (char c : str.toCharArray()) {
+      assertFalse(Character.isWhitespace(c));
+    }
+  }
+}
