9c6afbe4ee503515264e0885c607c1c68c541a6d
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-05-01 17:55:54 +0000

    added package-info.java in all packages.

---
+++ src/main/java/com/google/gson/reflect/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides support for finding type information for generic types
+ *  
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.google.gson.reflect;
---
+++ src/main/java/com/google/gson/version/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides versioning support for Gson
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.google.gson.version;
