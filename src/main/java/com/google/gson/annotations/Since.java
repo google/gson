e0a3ae5389855cef6ee694cfab49fe7b58efdb9e
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-05-09 22:35:26 +0000

    Created an "annotations" package to contain all GSON related annotations. In doing so, removed the "version" package.

--- src/main/java/com/google/gson/annotations/Since.java
+++ src/main/java/com/google/gson/annotations/Since.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.gson.version;
+package com.google.gson.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
---
+++ src/main/java/com/google/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with Gson.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.google.gson.annotations;
