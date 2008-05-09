5b60a25fe4b7e492cc36820abf58029d9dd5bc7a
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-05-09 21:01:50 +0000

    - Updated JavaDoc - Changed to a newer JUnit version

--- src/main/java/com/google/gson/package-info.java
+++ src/main/java/com/google/gson/package-info.java
@@ -1,10 +1,11 @@
 /**
  * This package provides a Java library for converting JSON to Java objects and
- * vice-versa
+ * vice-versa.
  * <p>
- * The primary class to use is {@link Gson} which can be constructed with 
- * {@code new Gson()} (using default settings) or by using {@link GsonBuilder} 
- * (to configure various options such as using versioning and so on).
+ * The primary class to use is {@link com.google.gson.Gson} which can be
+ * constructed with {@code new Gson()} (using default settings) or by using
+ * {@link com.google.gson.GsonBuilder} (to configure various options such as
+ * using versioning and so on).
  * 
  * @author Inderjeet Singh, Joel Leitch
  */
