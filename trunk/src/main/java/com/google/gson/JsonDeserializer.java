5a834e1deb59965b92633bbfa89cb9f89fc00465
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-05-15 00:57:08 +0000

    More javadocs

--- trunk/src/main/java/com/google/gson/JsonDeserializer.java
+++ trunk/src/main/java/com/google/gson/JsonDeserializer.java
@@ -19,14 +19,50 @@ package com.google.gson;
 import java.lang.reflect.Type;
 
 /**
- * Interface representing a custom deserializer for Json.
+ * Interface representing a custom deserializer for Json. You should write a custom deserializer, if 
+ * you are not happy with the default deserialization done by Gson. You will also need to register
+ * this deserializer through {@link GsonBuilder#registerTypeAdapter(Type, Object)}. 
+ * 
+ * <p>Let us look at example where defining a deserializer will be useful. The <code>Id</code> class 
+ * defined below has two fields: <code>clazz</code> and <code>value</code>. 
+ * 
+ * <p><code>
+ * public class Id&lt;T&gt; {<br>
+ *  &nbsp; private final Class&lt;T&gt; clazz;<br>
+ *  &nbsp; private final long value;<br>
+ *  &nbsp; public Id(Class&lt;T&gt; clazz, long value) {<br>
+ *  &nbsp; &nbsp; this.clazz = clazz;<br>
+ *  &nbsp; &nbsp; this.value = value;<br>
+ *  &nbsp; }<br>
+ *  &nbsp; public long getValue() {<br>
+ *  &nbsp; &nbsp; return value;<br>
+ *  &nbsp; }<br>
+ * }
+ * </code></p>
+ * 
+ * <p>The default deserialization of <code>Id(com.foo.MyObject.class, 20L)</code> will require the 
+ * Json string to be <code>{"clazz":com.foo.MyObject,"value":20}</code>. Suppose, you already know
+ * the type of the field that the <code>Id</code> will be deserialized into, and hence just want to 
+ * deserialize it from a Json string <code>20</code>. You can achieve that by writing a custom 
+ * deserializer:</p>
+ * 
+ * <p><code>
+ * class IdDeserializer implements JsonDeserializer&lt;Id&gt;() {<br>
+ *  &nbsp; public Id fromJson(JsonElement json, Type typeOfT, JsonDeserializationContext context) 
+ *  throws JsonParseException {<br>
+ *  &nbsp; &nbsp; return (Id) new Id((Class)typeOfT, id.getValue());<br>
+ *  }
+ * </code></p>   
+ * You will also need to register <code>IdDeserializer</code> with Gson as follows: 
+ * <p><code>
+ * Gson gson = new GsonBuilder().registerTypeAdapter(new IdDeserializer()).create();<br>
+ * </code></p>
  *
  * @author Inderjeet Singh
  * @author Joel Leitch
  *
- * @param <T> type for which the deserializer is being registered. It is
- * possible that a deserializer may be asked to deserialize a specific
- * generic type of the T.
+ * @param <T> type for which the deserializer is being registered. It is possible that a 
+ * deserializer may be asked to deserialize a specific generic type of the T.
  */
 public interface JsonDeserializer<T> {
   
