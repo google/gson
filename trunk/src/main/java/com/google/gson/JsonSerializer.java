05b83003be7491aa58892b3db24e93aac4271568
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-05-15 16:50:59 +0000

    Removed unused import that was showing up as a warning in Eclipse.

--- trunk/src/main/java/com/google/gson/JsonSerializer.java
+++ trunk/src/main/java/com/google/gson/JsonSerializer.java
@@ -17,16 +17,15 @@
 package com.google.gson;
 
 import java.lang.reflect.Type;
-import java.util.Locale;
 
 /**
- * Interface representing a custom serializer for Json. You should write a custom serializer, if 
+ * Interface representing a custom serializer for Json. You should write a custom serializer, if
  * you are not happy with the default serialization done by Gson. You will also need to register
- * this serializer through {@link GsonBuilder#registerTypeAdapter(Type, Object)}. 
- * 
- * <p>Let us look at example where defining a serializer will be useful. The <code>Id</code> class 
- * defined below has two fields: <code>clazz</code> and <code>value</code>. 
- * 
+ * this serializer through {@link GsonBuilder#registerTypeAdapter(Type, Object)}.
+ *
+ * <p>Let us look at example where defining a serializer will be useful. The <code>Id</code> class
+ * defined below has two fields: <code>clazz</code> and <code>value</code>.
+ *
  * <p><code>
  * public class Id&lt;T&gt; {<br>
  *  &nbsp; private final Class&lt;T&gt; clazz;<br>
@@ -40,38 +39,38 @@ import java.util.Locale;
  *  &nbsp; }<br>
  * }
  * </code></p>
- * 
- * <p>The default serialization of <code>Id(com.foo.MyObject.class, 20L)</code> will be 
- * <code>{"clazz":com.foo.MyObject,"value":20}</code>. Suppose, you just want the output to be the 
+ *
+ * <p>The default serialization of <code>Id(com.foo.MyObject.class, 20L)</code> will be
+ * <code>{"clazz":com.foo.MyObject,"value":20}</code>. Suppose, you just want the output to be the
  * value instead, which is 20 in this case. You can achieve that by writing a custom serializer:</p>
- * 
+ *
  * <p><code>
  * class IdSerializer implements JsonSerializer&lt;Id&gt;() {<br>
  *  &nbsp;  public JsonElement toJson(Id id, Type typeOfId, JsonSerializationContext context) {<br>
  *  &nbsp; &nbsp; return new JsonPrimitive(id.getValue());<br>
  *  }
- * </code></p>   
- * You will also need to register <code>IdSerializer</code> with Gson as follows: 
+ * </code></p>
+ * You will also need to register <code>IdSerializer</code> with Gson as follows:
  * <p><code>
  * Gson gson = new GsonBuilder().registerTypeAdapter(new IdSerializer()).create();<br>
  * </code></p>
- * 
+ *
  * @author Inderjeet Singh
  * @author Joel Leitch
  *
- * @param <T> type for which the serializer is being registered. It is possible that a serializer 
- * may be asked to serialize a specific generic type of the T. 
+ * @param <T> type for which the serializer is being registered. It is possible that a serializer
+ * may be asked to serialize a specific generic type of the T.
  */
 public interface JsonSerializer<T> {
-  
+
   /**
-   * Gson invokes this call-back method during serialization when it encounters a field of the 
+   * Gson invokes this call-back method during serialization when it encounters a field of the
    * specified type.
-   * 
+   *
    * @param src the object that needs to be converted to Json
-   * @param typeOfSrc the actual type (fully genericized version) of 
+   * @param typeOfSrc the actual type (fully genericized version) of
    *        the source object
-   * @return a JsonElement corresponding to the specified object 
+   * @return a JsonElement corresponding to the specified object
    */
   public JsonElement toJson(T src, Type typeOfSrc, JsonSerializationContext context);
 }
