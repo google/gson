61617258744a6ad2a50a60e2b21087557557df8c
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-07-26 05:17:33 +0000

    Fix the pretty printer (bug #22).  Added tests to expose this bug.

--- src/test/java/com/google/gson/JsonPrintFormatterTest.java
+++ src/test/java/com/google/gson/JsonPrintFormatterTest.java
@@ -24,12 +24,13 @@ import com.google.gson.reflect.TypeToken;
 import junit.framework.TestCase;
 
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Small test for {@link JsonPrintFormatter}
- * 
+ *
  * @author Inderjeet Singh
  */
 public class JsonPrintFormatterTest extends TestCase {
@@ -65,6 +66,30 @@ public class JsonPrintFormatterTest extends TestCase {
     String json = gson.toJson(target);
     print(json);
     assertPrintMargin(json);
+  }
+
+  public void testArrayOfPrimitives() {
+    int[] ints = new int[] { 1, 2, 3, 4, 5 };
+    String json = gson.toJson(ints);
+    assertEquals("[1,2,3,4,5]\n", json);
+  }
+
+  public void testArrayOfPrimitiveArrays() {
+    int[][] ints = new int[][] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 0 }, { 10 } };
+    String json = gson.toJson(ints);
+    assertEquals("[[1,2],[3,4],[5,6],[7,8],[9,0],[10]]\n", json);
+  }
+
+  public void testListOfPrimitiveArrays() {
+    List<Integer[]> list = Arrays.asList(new Integer[][] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 0 }, { 10 } });
+    String json = gson.toJson(list);
+    assertEquals("[[1,2],[3,4],[5,6],[7,8],[9,0],[10]]\n", json);
+  }
+
+  public void testMultipleArrays() {
+    int[][][] ints = new int[][][] { {  { 1 }, { 2 } } };
+    String json = gson.toJson(ints);
+    assertEquals("[[[1],[2]]]\n", json);
   }
 
   private void print(String msg) {
