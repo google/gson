d3cea039c379291e3c04182dbc3ac02e4d1464e8
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-07-31 02:02:25 +0000

    added a test to test custom serialization using JsonNull.

--- trunk/src/test/java/com/google/gson/NullSerializationTests.java
+++ trunk/src/test/java/com/google/gson/NullSerializationTests.java
@@ -1,25 +1,44 @@
 package com.google.gson;
 
+import java.lang.reflect.Type;
+
 import junit.framework.TestCase;
 
+import com.google.gson.TestTypes.BagOfPrimitives;
 import com.google.gson.TestTypes.ClassWithObjects;
 
 public class NullSerializationTests extends TestCase {
-  private Gson gson;
+  private GsonBuilder gsonBuilder;
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();    
-    gson = new GsonBuilder()
-        .serializeNulls()
-        .create();
+    gsonBuilder = new GsonBuilder().serializeNulls();
   }
   
   public void testExplicitSerializationOfNulls() {
+    Gson gson = gsonBuilder.create();
     ClassWithObjects target = new ClassWithObjects(null);
     String actual = gson.toJson(target);
     String expected = "{\"bag\":null}";
     assertEquals(expected, actual);
   }
-
+  
+  private static class ClassWithObjectsSerializer implements JsonSerializer<ClassWithObjects> {
+    public JsonElement serialize(ClassWithObjects src, Type typeOfSrc, 
+        JsonSerializationContext context) {
+      JsonObject obj = new JsonObject();
+      obj.add("bag", new JsonNull());
+      return obj;
+    }    
+  }
+  
+  public void testCustomSerializationOfNulls() {
+    gsonBuilder.registerSerializer(ClassWithObjects.class, new ClassWithObjectsSerializer());
+    Gson gson = gsonBuilder.create();
+    ClassWithObjects target = new ClassWithObjects(new BagOfPrimitives());
+    String actual = gson.toJson(target);
+    String expected = "{\"bag\":null}";
+    assertEquals(expected, actual);
+  }
 }
