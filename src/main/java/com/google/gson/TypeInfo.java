b7a2e5ed387cdf357a122a7b1324aa8d6a785f9f
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-08-13 23:25:32 +0000

    missed this file.

---
+++ src/main/java/com/google/gson/TypeInfo.java
@@ -0,0 +1,82 @@
+package com.google.gson;
+
+import java.lang.reflect.Type;
+import java.util.Collection;
+
+/**
+ * Class that provides information relevant to different parts of a type.
+ *
+ * @author Inderjeet Singh
+ * @author Joel Leitch
+ */
+class TypeInfo {
+  protected final Type actualType;
+  protected final Class<?> rawClass;
+
+  TypeInfo(Type actualType) {
+    this.actualType = actualType;
+    rawClass = TypeUtils.toRawClass(actualType);
+  }
+
+  public final Type getActualType() {
+    return actualType;
+  }
+
+  /**
+   * Returns the actual type matching up with the first type variable.
+   * So, for
+   * <pre>
+   *   class Foo<A, B> {
+   *   }
+   * new Foo<Integer, String>{}
+   * </pre>, it will return Integer.class.
+   */
+  public final Type getActualTypeForFirstTypeVariable() {
+    Type genericClass = TypeUtils.getActualTypeForFirstTypeVariable(actualType);
+    return genericClass;
+  }
+
+  /**
+   * Returns the corresponding wrapper type of {@code type} if it is a primitive
+   * type; otherwise returns {@code type} itself. Idempotent.
+   * <pre>
+   *     wrap(int.class) == Integer.class
+   *     wrap(Integer.class) == Integer.class
+   *     wrap(String.class) == String.class
+   * </pre>
+   */
+  public Class<?> getWrappedClass() {
+    return Primitives.wrap(rawClass);
+  }
+
+  /**
+   * @return the raw class associated with this type
+   */
+  public final Class<?> getRawClass() {
+    return rawClass;
+  }
+
+  public final boolean isCollectionOrArray() {
+    return Collection.class.isAssignableFrom(rawClass) || isArray();
+  }
+
+  public final boolean isArray() {
+    return TypeUtils.isArray(rawClass);
+  }
+
+  public final boolean isEnum() {
+    return rawClass.isEnum();
+  }
+
+  public final boolean isPrimitive() {
+    return Primitives.isWrapperType(Primitives.wrap(rawClass));
+  }
+
+  public final boolean isString() {
+    return rawClass == String.class;
+  }
+
+  public final boolean isPrimitiveOrStringAndNotAnArray() {
+    return (isPrimitive() || isString()) && !isArray();
+  }
+}
