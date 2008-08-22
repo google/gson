c2fdb67dcf43d25bdd7b9213591d6bc5a898315f
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-08-22 22:26:05 +0000

    Moved tests for default type adapters into DefaultTypeAdaptersTest. Moved object tests into ObjectTest.

--- src/test/java/com/google/gson/JsonDeserializerTest.java
+++ src/test/java/com/google/gson/JsonDeserializerTest.java
@@ -16,37 +16,16 @@
 
 package com.google.gson;
 
-import com.google.gson.TestTypes.ArrayOfArrays;
-import com.google.gson.TestTypes.ArrayOfObjects;
 import com.google.gson.TestTypes.BagOfPrimitives;
-import com.google.gson.TestTypes.ClassWithArray;
 import com.google.gson.TestTypes.ClassWithCustomTypeConverter;
-import com.google.gson.TestTypes.ClassWithEnumFields;
-import com.google.gson.TestTypes.ClassWithNoFields;
-import com.google.gson.TestTypes.ClassWithObjects;
-import com.google.gson.TestTypes.ClassWithPrivateNoArgsConstructor;
 import com.google.gson.TestTypes.ClassWithSerializedNameFields;
-import com.google.gson.TestTypes.ClassWithSubInterfacesOfCollection;
-import com.google.gson.TestTypes.ClassWithTransientFields;
-import com.google.gson.TestTypes.ContainsReferenceToSelfType;
-import com.google.gson.TestTypes.ExceptionHolder;
-import com.google.gson.TestTypes.MyEnum;
-import com.google.gson.TestTypes.Nested;
-import com.google.gson.TestTypes.PrimitiveArray;
 import com.google.gson.TestTypes.StringWrapper;
-import com.google.gson.TestTypes.SubTypeOfNested;
-import com.google.gson.reflect.TypeToken;
 
 import junit.framework.TestCase;
 
 import java.io.Reader;
 import java.io.StringReader;
 import java.lang.reflect.Type;
-import java.net.URI;
-import java.net.URL;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.Map;
 
 /**
  * Small test for Json Deserialization.
@@ -83,96 +62,6 @@ public class JsonDeserializerTest extends TestCase {
     assertEquals(expected, actual);
   }
 
-  @SuppressWarnings("unchecked")
-  public void testRawCollectionOfBagOfPrimitives() {
-    try {
-      BagOfPrimitives bag = new BagOfPrimitives(10, 20, false, "stringValue");
-      String json = '[' + bag.getExpectedJson() + ',' + bag.getExpectedJson() + ']';
-      Collection target = gson.fromJson(json, Collection.class);
-      assertEquals(2, target.size());
-      for (BagOfPrimitives bag1 : (Collection<BagOfPrimitives>) target) {
-        assertEquals(bag.getExpectedJson(), bag1.getExpectedJson());
-      }
-      fail("Raw collection of objects should not work");
-    } catch (JsonParseException expected) {
-    }
-  }
-
-  public void testEmptyCollectionInAnObject() {
-    String json = "{\"children\":[]}";
-    ContainsReferenceToSelfType target = gson.fromJson(json, ContainsReferenceToSelfType.class);
-    assertNotNull(target);
-    assertTrue(target.children.isEmpty());
-  }
-
-  public void testPrimitiveArrayInAnObject() {
-    String json = "{\"longArray\":[0,1,2,3,4,5,6,7,8,9]}";
-    PrimitiveArray target = gson.fromJson(json, PrimitiveArray.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testClassWithTransientFields() throws Exception {
-    String json = "{\"longValue\":[1]}";
-    ClassWithTransientFields target = gson.fromJson(
-        json, ClassWithTransientFields.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testTransientFieldsPassedInJsonAreIgnored() throws Exception {
-    String json = "{\"transientLongValue\":1,\"longValue\":[1]}";
-    ClassWithTransientFields target = gson.fromJson(
-        json, ClassWithTransientFields.class);
-    assertFalse(target.transientLongValue != 1);
-  }
-
-  public void testClassWithNoFields() {
-    String json = "{}";
-    ClassWithNoFields target = gson.fromJson(json, ClassWithNoFields.class);
-    assertNotNull(target);
-  }
-
-  public void testNested() {
-    String json = "{\"primitive1\":{\"longValue\":10,\"intValue\":20,\"booleanValue\":false,"
-        + "\"stringValue\":\"stringValue\"},\"primitive2\":{\"longValue\":30,\"intValue\":40,"
-        + "\"booleanValue\":true,\"stringValue\":\"stringValue\"}}";
-    Nested target = gson.fromJson(json, Nested.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testInheritence() {
-    String json = "{\"value\":5,\"primitive1\":{\"longValue\":10,\"intValue\":20,"
-        + "\"booleanValue\":false,\"stringValue\":\"stringValue\"},\"primitive2\":"
-        + "{\"longValue\":30,\"intValue\":40,\"booleanValue\":true,"
-        + "\"stringValue\":\"stringValue\"}}";
-    SubTypeOfNested target = gson.fromJson(json, SubTypeOfNested.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testNull() {
-    try {
-      gson.fromJson("", Object.class);
-      fail("Null strings should not be allowed");
-    } catch (JsonParseException expected) {
-    }
-  }
-
-  public void testNullFields() {
-    String json = "{\"primitive1\":{\"longValue\":10,\"intValue\":20,\"booleanValue\":false"
-        + ",\"stringValue\":\"stringValue\"}}";
-    Nested target = gson.fromJson(json, Nested.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testSubInterfacesOfCollection() {
-    String json = "{\"list\":[0,1,2,3],\"queue\":[0,1,2,3],\"set\":[0.1,0.2,0.3,0.4],"
-        + "\"sortedSet\":[\"a\",\"b\",\"c\",\"d\"]"
-//        + ",\"navigableSet\":[\"abc\",\"def\",\"ghi\",\"jkl\"]"
-        + "}";
-    ClassWithSubInterfacesOfCollection target = gson.fromJson(
-        json, ClassWithSubInterfacesOfCollection.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
   public void testCustomDeserializer() {
     Gson gson = new GsonBuilder().registerDeserializer(
         ClassWithCustomTypeConverter.class, new JsonDeserializer<ClassWithCustomTypeConverter>() {
@@ -203,111 +92,6 @@ public class JsonDeserializerTest extends TestCase {
     assertEquals(7, target.getBag().getIntValue());
   }
 
-  public void testArrayOfObjects() {
-    String json = new ArrayOfObjects().getExpectedJson();
-    ArrayOfObjects target = gson.fromJson(json, ArrayOfObjects.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testArrayOfArrays() {
-    String json = new ArrayOfArrays().getExpectedJson();
-    ArrayOfArrays target = gson.fromJson(json, ArrayOfArrays.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testTopLevelEnum() {
-    String json = MyEnum.VALUE1.getExpectedJson();
-    MyEnum target = gson.fromJson(json, MyEnum.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testTopLevelEnumInASingleElementArray() {
-    String json = "[" + MyEnum.VALUE1.getExpectedJson() + "]";
-    MyEnum target = gson.fromJson(json, MyEnum.class);
-    assertEquals(json, "[" + target.getExpectedJson() + "]");
-  }
-
-  public void testClassWithEnumField() {
-    String json = new ClassWithEnumFields().getExpectedJson();
-    ClassWithEnumFields target = gson.fromJson(json, ClassWithEnumFields.class);
-    assertEquals(json, target.getExpectedJson());
-  }
-
-  public void testPrivateNoArgConstructor() {
-    ClassWithPrivateNoArgsConstructor target =
-      gson.fromJson("{\"a\":20}", ClassWithPrivateNoArgsConstructor.class);
-    assertEquals(20, target.a);
-  }
-
-  public void testDefaultSupportForUrl() throws Exception {
-    String urlValue = "http://google.com/";
-    String json = '"' + urlValue + '"';
-    URL target = gson.fromJson(json, URL.class);
-    assertEquals(urlValue, target.toExternalForm());
-  }
-
-  public void testDefaultSupportForUri() throws Exception {
-    String uriValue = "http://google.com/";
-    String json = '"' + uriValue + '"';
-    URI target = gson.fromJson(json, URI.class);
-    assertEquals(uriValue, target.toASCIIString());
-  }
-
-  public void testDefaultSupportForLocaleWithLanguage() throws Exception {
-    String json = "\"en\"";
-    Locale locale = gson.fromJson(json, Locale.class);
-    assertEquals("en", locale.getLanguage());
-  }
-
-  public void testDefaultSupportForLocaleWithLanguageCountry() throws Exception {
-    String json = "\"fr_CA\"";
-    Locale locale = gson.fromJson(json, Locale.class);
-    assertEquals(Locale.CANADA_FRENCH, locale);
-  }
-
-  public void testDefaultSupportForLocaleWithLanguageCountryVariant() throws Exception {
-    String json = "\"de_DE_EURO\"";
-    Locale locale = gson.fromJson(json, Locale.class);
-    assertEquals("de", locale.getLanguage());
-    assertEquals("DE", locale.getCountry());
-    assertEquals("EURO", locale.getVariant());
-  }
-
-  public void testMap() throws Exception {
-    String json = "{\"a\":1,\"b\":2}";
-    Type typeOfMap = new TypeToken<Map<String,Integer>>(){}.getType();
-    Map<String, Integer> target = gson.fromJson(json, typeOfMap);
-    assertEquals(1, target.get("a").intValue());
-    assertEquals(2, target.get("b").intValue());
-  }
-
-  /**
-   * Created in response to Issue 14: http://code.google.com/p/google-gson/issues/detail?id=14
-   */
-  public void testNullArrays() {
-    String json = "{\"array\": null}";
-    ClassWithArray target = gson.fromJson(json, ClassWithArray.class);
-    assertNull(target.array);
-  }
-
-  /**
-   * Created in response to Issue 14: http://code.google.com/p/google-gson/issues/detail?id=14
-   */
-  public void testNullObjectFields() {
-    String json = "{\"bag\": null}";
-    ClassWithObjects target = gson.fromJson(json, ClassWithObjects.class);
-    assertNull(target.bag);
-  }
-
-  /**
-   * Created in response to Issue 14: http://code.google.com/p/google-gson/issues/detail?id=14
-   */
-  public void testNullPrimitiveFields() {
-    String json = "{\"longValue\":null}";
-    BagOfPrimitives target = gson.fromJson(json, BagOfPrimitives.class);
-    assertEquals(BagOfPrimitives.DEFAULT_VALUE, target.longValue);
-  }
-
   public void testGsonWithNonDefaultFieldNamingPolicy() {
     Gson gson = new GsonBuilder().setFieldNamingPolicy(
         FieldNamingPolicy.UPPER_CAMEL_CASE).create();
@@ -323,35 +107,5 @@ public class JsonDeserializerTest extends TestCase {
     ClassWithSerializedNameFields actual =
         gson.fromJson(expected.getExpectedJson(), ClassWithSerializedNameFields.class);
     assertEquals(expected.f, actual.f);
-  }
-
-  public void testReallyLongStrings() {
-    StringBuilder sb = new StringBuilder(8096);
-    sb.append("Once upon a time there was a really long string that caused a StackOverFlowError\n");
-    sb.append("and now it is fixed and instead throws a JsonParserException.....Yippie!!!\n");
-    sb.append("Wow....that is a really long string that is mean to be an exception, but is not :( \n.");
-    sb.append("C'est la vie!!! \n\n\n\n\n");
-
-    for (int i = 0; i < 10; i++) {
-      sb.append(sb.toString());
-    }
-
-    while (true) {
-      try {
-        String stackTrace = sb.toString();
-        sb.append(stackTrace);
-        String json = "{\"message\":\"Error message.\","
-          + "\"stackTrace\":\"" + stackTrace + "\"}";
-        parseLongJson(json);
-      } catch (JsonParseException expected) {
-        break;
-      }
-    }
-  }
-
-  private void parseLongJson(String json) throws JsonParseException {
-    ExceptionHolder target = gson.fromJson(json, ExceptionHolder.class);
-    assertTrue(target.message.contains("Error"));
-    assertTrue(target.stackTrace.contains("Yippie"));
   }
 }
