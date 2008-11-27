a102dcb25a51ee17beaf6e5ed7c9d6b9ea6f3bf6
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2008-11-27 00:50:50 +0000

    Do not overwrite a duplicate field value during seriailzation if one those fields is null.

--- gson/src/test/java/com/google/gson/functional/NamingPolicyTest.java
+++ gson/src/test/java/com/google/gson/functional/NamingPolicyTest.java
@@ -18,6 +18,7 @@ package com.google.gson.functional;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 import com.google.gson.common.TestTypes.ClassWithSerializedNameFields;
 import com.google.gson.common.TestTypes.StringWrapper;
 
@@ -68,5 +69,23 @@ public class NamingPolicyTest extends TestCase {
     ClassWithSerializedNameFields actual =
         gson.fromJson(expected.getExpectedJson(), ClassWithSerializedNameFields.class);
     assertEquals(expected.f, actual.f);
+  }
+  
+  public void testGsonDuplicateNameUsingSerializedNameFieldNamingPolicySerialization() {
+    Gson gson = builder.create();
+    ClassWithDuplicateFields target = new ClassWithDuplicateFields();
+    target.a = 10;
+    String actual = gson.toJson(target);
+    assertEquals("{\"a\":10}", actual);
+    
+    target.a = null;
+    target.b = 3.0D;
+    actual = gson.toJson(target);
+    assertEquals("{\"a\":3.0}", actual);
+  }
+  
+  private static class ClassWithDuplicateFields {
+    private Integer a;
+    @SerializedName("a") private Double b;
   }
 }
