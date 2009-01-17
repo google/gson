47b67390fa5816e1a8d158c379c0e9470efd8442
Joel Leitch <joel.leitch@gmail.com>
2009-01-17 22:34:57 +0000

    Set the DEBUG bit back to false.

--- gson/src/test/java/com/google/gson/functional/PrettyPrintingTest.java
+++ gson/src/test/java/com/google/gson/functional/PrettyPrintingTest.java
@@ -40,7 +40,7 @@ public class PrettyPrintingTest extends TestCase {
   private static int PRINT_MARGIN = 80;
   private static int RIGHT_MARGIN = 4;
 
-  private static boolean DEBUG = true;
+  private static boolean DEBUG = false;
 
   private Gson gson;
 
