3e1c17d3ee22dce347a73329be862a0c655c2381
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2009-01-29 02:43:36 +0000

    updated javadoc in response to issue 97

--- trunk/gson/src/main/java/com/google/gson/JsonNull.java
+++ trunk/gson/src/main/java/com/google/gson/JsonNull.java
@@ -59,7 +59,10 @@ public final class JsonNull extends JsonElement {
   /**
    * Creation method used to return an instance of a {@link JsonNull}.  To reduce the memory
    * footprint, a single object has been created for this class; therefore the same instance is
-   * being returned for each invocation of this method.
+   * being returned for each invocation of this method. This method is kept private since we 
+   * prefer the users to use {@link JsonNull#JsonNull()} which is similar to how other JsonElements
+   * are created. Note that all instances of JsonNull return true for {@link #equals(Object)} 
+   * when compared to each other.
    *
    * @return a instance of a {@link JsonNull}
    */
