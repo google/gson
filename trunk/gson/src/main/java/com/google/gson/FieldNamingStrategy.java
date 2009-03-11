83ebdc3c0090972635ec7fe3c407a8f79f807f5c
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2009-03-11 21:53:02 +0000

    Fixed Issue 104 by making FieldNamingStrategy to be public and allowing FieldNamingStrategy to be set in GsonBuilder

--- trunk/gson/src/main/java/com/google/gson/FieldNamingStrategy.java
+++ trunk/gson/src/main/java/com/google/gson/FieldNamingStrategy.java
@@ -24,14 +24,16 @@ import java.lang.reflect.Field;
  * declaration rules.  For example, Java does not support "-" characters in a field name.
  *
  * @author Joel Leitch
+ * @since 1.3
  */
-interface FieldNamingStrategy {
+public interface FieldNamingStrategy {
 
   /**
    * Translates the field name into its JSON field name representation.
    *
    * @param f the field object that we are translating
    * @return the translated field name.
+   * @since 1.3
    */
   public String translateName(Field f);
 }
