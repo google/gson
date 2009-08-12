1ad473de6c54ac82fc75f15b37d01fa1f86faf09
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2009-08-12 21:01:42 +0000

    Made JsonParser implement Iterator

--- trunk/gson/src/test/java/com/google/gson/JsonParserAsyncTest.java
+++ trunk/gson/src/test/java/com/google/gson/JsonParserAsyncTest.java
@@ -1,5 +1,8 @@
 package com.google.gson;
 
+import java.util.Enumeration;
+import java.util.Iterator;
+
 import junit.framework.TestCase;
 
 /**
@@ -11,9 +14,18 @@ public class JsonParserAsyncTest extends TestCase {
   
   public void testParseTwoStrings() {
     JsonParserAsync parser = new JsonParserAsync("'one' 'two'");
-    String actualOne = parser.nextElement().getAsString();
+    String actualOne = parser.next().getAsString();
     assertEquals("one", actualOne);
-    String actualTwo = parser.nextElement().getAsString();
+    String actualTwo = parser.next().getAsString();
     assertEquals("two", actualTwo);
+  }
+  
+  public void testIterator() {
+    Iterator<JsonElement> parser = new JsonParserAsync("'one' 'two'");
+    assertTrue(parser.hasNext());
+    assertEquals("one", parser.next().getAsString());
+    assertTrue(parser.hasNext());
+    assertEquals("two", parser.next().getAsString());
+    assertFalse(parser.hasNext());
   }
 }
