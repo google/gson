43a5eb45380a222c15660cfc61bb6aea7de9730a
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-07-16 16:06:41 +0000

    Added an example client to invoke example web-service Update license files

---
+++ trunk/wsexample/client/src/main/java/com/google/gson/example/client/ExampleClient.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.gson.example.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import com.google.gson.example.model.Cart;
+import com.google.gson.example.model.LineItem;
+import com.google.gson.example.model.Order;
+import com.google.gson.example.model.TypedKeys;
+import com.google.gson.example.service.SampleJsonService;
+import com.google.gson.webservice.client.WebServiceClient;
+import com.google.gson.webservice.client.WebServiceConfig;
+import com.google.gson.webservice.definition.HeaderMap;
+import com.google.gson.webservice.definition.HttpMethod;
+import com.google.gson.webservice.definition.RequestBody;
+import com.google.gson.webservice.definition.WebServiceCallSpec;
+import com.google.gson.webservice.definition.WebServiceRequest;
+import com.google.gson.webservice.definition.WebServiceResponse;
+
+public class ExampleClient {
+
+  private final WebServiceClient wsClient;
+  public ExampleClient() {
+    WebServiceConfig serverConfig = new WebServiceConfig("http://localhost");
+	wsClient = new WebServiceClient(serverConfig, Level.INFO); 
+  }
+
+  public Order placeOrder(Cart cart, String authToken) {
+    WebServiceCallSpec spec = SampleJsonService.PLACE_ORDER;
+	HeaderMap requestHeaders = new HeaderMap.Builder(spec.getRequestSpec().getHeadersSpec())
+	    .put(TypedKeys.Request.AUTH_TOKEN, authToken)
+	    .build();
+	RequestBody requestBody = new RequestBody.Builder(spec.getRequestSpec().getBodySpec())
+	    .put(TypedKeys.RequestBody.CART, cart)
+	    .build();
+	WebServiceRequest request = new WebServiceRequest(HttpMethod.POST, requestHeaders, requestBody);
+	WebServiceResponse response = wsClient.getResponse(spec, request);
+	return response.getBody().get(TypedKeys.ResponseBody.ORDER);
+  }
+
+  public static void main(String[] args) {
+    ExampleClient client = new ExampleClient();
+    List<LineItem> lineItems = new ArrayList<LineItem>();
+    lineItems.add(new LineItem("item1", 2, 1000000L, "USD"));
+	Cart cart = new Cart(lineItems, "first last", "4111-1111-1111-1111");
+	String authToken = "authToken";
+	client.placeOrder(cart, authToken );
+  }
+}
