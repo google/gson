f4b340372af396968bd7cbe7d051827e29ae8a9d
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-07-20 00:34:40 +0000

    fixed copyright notice, minor cosmetic changes.

--- trunk/wsexample/server/src/main/java/com/google/gson/wsexample/server/MainServlet.java
+++ trunk/wsexample/server/src/main/java/com/google/gson/wsexample/server/MainServlet.java
@@ -1,5 +1,18 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.gson.wsexample.server;
 
 import com.google.gson.Gson;
@@ -33,11 +46,9 @@ import javax.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet {
   @Override
   public void service(HttpServletRequest req, HttpServletResponse res) {
-    // construct specs
     WebServiceCallSpec spec = SampleJsonService.PLACE_ORDER;
     RequestSpec requestSpec = spec.getRequestSpec();
     ResponseSpec responseSpec = spec.getResponseSpec();
-    // construct gson instance
     Gson gson = new GsonBuilder()
         .registerTypeAdapter(RequestBody.class,
             new RequestBodyGsonConverter(requestSpec.getBodySpec()))
@@ -46,11 +57,13 @@ public class MainServlet extends HttpServlet {
         .create();
     RequestReceiver requestReceiver = new RequestReceiver(gson, requestSpec);
     WebServiceRequest webServiceRequest = requestReceiver.receive(req);
-    RequestBody requestBody = webServiceRequest.getBody();
-    Cart cart = requestBody.get(TypedKeys.RequestBody.CART);
+
+    Cart cart = webServiceRequest.getBody().get(TypedKeys.RequestBody.CART);
     String authToken = webServiceRequest.getHeader(TypedKeys.Request.AUTH_TOKEN);
+
     Order order = placeOrder(cart, authToken);
 
+    // Empty headers per the spec
     HeaderMap responseHeaders = new HeaderMap.Builder(responseSpec.getHeadersSpec()).build();
     ResponseBody responseBody = new ResponseBody.Builder(responseSpec.getBodySpec())
         .put(TypedKeys.ResponseBody.ORDER, order)
@@ -61,6 +74,7 @@ public class MainServlet extends HttpServlet {
   }
   
   private Order placeOrder(Cart cart, String authToken) {
+    // Create an order, in this case a dummy one.
     return new Order(cart, "Order123");
   }
 }
