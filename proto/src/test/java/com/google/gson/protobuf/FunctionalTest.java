9b0e7f8b84298b284fe79aaa317ed36394921a24
Inderjeet Singh <inder123@gmail.com>
2010-09-14 17:59:04 +0000

    initial check-in for implementing support for protocol buffers in Gson

---
+++ proto/src/test/java/com/google/gson/protobuf/FunctionalTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.gson.protobuf;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.protobuf.generated.Bag.SimpleProto;
+import com.google.protobuf.Message;
+import com.google.protobuf.Descriptors.Descriptor;
+
+import junit.framework.TestCase;
+
+public class FunctionalTest extends TestCase {
+  private Gson gson;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    gson = new GsonBuilder()
+//      .registerTypeHierarchyAdapter(Message.class, new ProtoTypeAdapter())
+      .create();
+  }
+
+  public void testSerializeEmptyProto() {
+    SimpleProto proto = SimpleProto.newBuilder().build();
+    String json = gson.toJson(proto);
+    assertEquals("{}", json);
+  }
+
+  public void testSerializeProto() {
+    Descriptor descriptor = SimpleProto.getDescriptor();
+    SimpleProto proto = SimpleProto.newBuilder()
+      .setField(descriptor.findFieldByNumber(1), "foo")
+      .setField(descriptor.findFieldByNumber(2), 3)
+      .build();
+    String json = gson.toJson(proto);
+    System.out.println(json);
+    assertTrue(json.contains("\"msg\":\"foo\""));
+    assertTrue(json.contains("\"count\":3"));
+  }
+}
