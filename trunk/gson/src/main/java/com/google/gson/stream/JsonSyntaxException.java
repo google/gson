757cdb8f542490c3570cba5e8103104c8b4831ad
limpbizkit <limpbizkit@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-10-30 21:32:08 +0000

    Make JsonSyntaxException public, so clients can differentiate between IO problems and malformed JSON.

---
+++ trunk/gson/src/main/java/com/google/gson/stream/JsonSyntaxException.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gson.stream;
+
+import java.io.IOException;
+
+/**
+ * Thrown when a reader encounters malformed JSON. Some syntax errors can be
+ * ignored by calling {@link JsonReader#setLenient(boolean)}.
+ */
+public final class JsonSyntaxException extends IOException {
+  public JsonSyntaxException(String s) {
+    super(s);
+  }
+}
