8f5660b8cf5db3c07a5fb7969dad5d16c843cc90
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-11-01 23:10:09 +0000

    removed unused JsonFormatter classes.

--- trunk/gson/src/main/java/com/google/gson/Escaper.java
+++ trunk/gson/src/main/java/com/google/gson/Escaper.java
@@ -34,7 +34,7 @@ import java.util.Set;
  * @author Inderjeet Singh
  * @author Joel Leitch
  */
-class Escaper {
+final class Escaper {
 
   private static final char[] HEX_CHARS = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
