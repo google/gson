cf026212758bc4f18ee3946c0931e68d1ac99529
Inderjeet Singh <inder123@gmail.com>
2010-11-02 00:12:21 +0000

    removed unused import

--- proto/src/main/java/com/google/gson/protobuf/ProtoTypeAdapter.java
+++ proto/src/main/java/com/google/gson/protobuf/ProtoTypeAdapter.java
@@ -14,7 +14,6 @@ import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.GeneratedMessage;
 
-import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
