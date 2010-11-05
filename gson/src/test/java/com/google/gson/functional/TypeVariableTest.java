752522b89b8e7f8a55e5d01923f4c756a6b69530
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-11-05 22:57:44 +0000

    Adding new tests (two of which are disabled, but prepping for 1.7).

--- gson/src/test/java/com/google/gson/functional/TypeVariableTest.java
+++ gson/src/test/java/com/google/gson/functional/TypeVariableTest.java
@@ -26,16 +26,19 @@ import java.util.Map;
 
 /**
  * Functional test for Gson serialization and deserialization of
+ * classes with type variables.
+ *
  * @author Joel Leitch
  */
 public class TypeVariableTest extends TestCase {
 
-  public void testSingle() throws Exception {
+  public void disabled_testAdvancedTypeVariables() throws Exception {
     Gson gson = new Gson();
-    Bar bar1 = new Bar("someString", 1);
+    Bar bar1 = new Bar("someString", 1, true);
     ArrayList<Integer> arrayList = new ArrayList<Integer>();
     arrayList.add(1);
     arrayList.add(2);
+    arrayList.add(3);
     bar1.map.put("key1", arrayList);
     bar1.map.put("key2", new ArrayList<Integer>());
     String json = gson.toJson(bar1);
@@ -44,36 +47,80 @@ public class TypeVariableTest extends TestCase {
     assertEquals(bar1, bar2);
   }
 
-  public static class Foo<S, T> {
+  public void testBasicTypeVariables() throws Exception {
+    Gson gson = new Gson();
+    Blue blue1 = new Blue(true);
+    String json = gson.toJson(blue1);
+
+    Blue blue2 = gson.fromJson(json, Blue.class);
+    assertEquals(blue1, blue2);
+  }
+
+  public static class Blue extends Red<Boolean> {
+    public Blue() {
+      super(false);
+    }
+
+    public Blue(boolean value) {
+      super(value);
+    }
+
+    // Technically, we should implement hashcode too
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Blue)) {
+        return false;
+      } else {
+        Blue blue = (Blue) o;
+        return redField.equals(blue.redField);
+
+      }
+    }
+  }
+
+  public static class Red<S> {
+    protected final S redField;
+
+    public Red(S redField) {
+      this.redField = redField;
+    }
+  }
+
+  public static class Foo<S, T> extends Red<Boolean> {
     private final S someSField;
     private final T someTField;
     public final Map<S, List<T>> map = new HashMap<S, List<T>>();
 
-    public Foo(S sValue, T tValue) {
+    public Foo(S sValue, T tValue, Boolean redField) {
+      super(redField);
       this.someSField = sValue;
       this.someTField = tValue;
     }
 
-    @SuppressWarnings("unchecked")
+    // Technically, we should implement hashcode too
     @Override
+    @SuppressWarnings("unchecked")
     public boolean equals(Object o) {
-      if (!(o instanceof Foo)) {
+      if (!(o instanceof Foo<?, ?>)) {
         return false;
+      } else {
+        Foo<S, T> realFoo = (Foo<S, T>) o;
+        return redField.equals(realFoo.redField)
+            && someTField.equals(realFoo.someTField)
+            && someSField.equals(realFoo.someSField)
+            && map.equals(realFoo.map);
+
       }
-      Foo<S, T> realFoo = (Foo<S, T>) o;
-      return someTField.equals(realFoo.someTField)
-        && someSField.equals(realFoo.someSField)
-        && map.equals(realFoo.map);
     }
   }
 
   public static class Bar extends Foo<String, Integer> {
     public Bar() {
-      this("", 0);
+      this("", 0, false);
     }
 
-    public Bar(String s, Integer i) {
-      super(s, i);
+    public Bar(String s, Integer i, boolean b) {
+      super(s, i, b);
     }
   }
 }
