c1a3346a3385cd1f9650e558355701e6c30598d4
limpbizkit <limpbizkit@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2010-11-05 20:58:41 +0000

    New overloads for constructing MalformedJsonException

--- trunk/gson/src/main/java/com/google/gson/stream/MalformedJsonException.java
+++ trunk/gson/src/main/java/com/google/gson/stream/MalformedJsonException.java
@@ -25,7 +25,16 @@ import java.io.IOException;
 public final class MalformedJsonException extends IOException {
   private static final long serialVersionUID = 1L;
 
-  public MalformedJsonException(String s) {
-    super(s);
+  public MalformedJsonException(String msg) {
+    super(msg);
+  }
+
+  public MalformedJsonException(String msg, Throwable throwable) {
+    super(msg);
+    initCause(throwable);
+  }
+
+  public MalformedJsonException(Throwable throwable) {
+    initCause(throwable);
   }
 }
