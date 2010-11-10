32afd1a4e45e4ae42bb6bceb8a5823dfe8b44fa2
Joel Leitch <joel.leitch@gmail.com>
2010-11-10 02:02:57 +0000

    Fixing parsing of unquoted strings to be (somewhat) consistent with previous versions of Gson. The difference with this version is that Gson will throw a more specific exception rather than JsonParseException.

--- gson/src/main/java/com/google/gson/JsonParser.java
+++ gson/src/main/java/com/google/gson/JsonParser.java
@@ -16,41 +16,58 @@
 package com.google.gson;
 
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.MalformedJsonException;
+
 import java.io.EOFException;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
 /**
  * A parser to parse Json into a parse tree of {@link JsonElement}s
- * 
+ *
  * @author Inderjeet Singh
  * @author Joel Leitch
  * @since 1.3
  */
 public final class JsonParser {
-  
+
   /**
    * Parses the specified JSON string into a parse tree
-   * 
+   *
    * @param json JSON text
-   * @return a parse tree of {@link JsonElement}s corresponding to the specified JSON 
+   * @return a parse tree of {@link JsonElement}s corresponding to the specified JSON
    * @throws JsonParseException if the specified text is not valid JSON
    * @since 1.3
    */
   public JsonElement parse(String json) throws JsonSyntaxException {
     return parse(new StringReader(json));
   }
-  
+
   /**
    * Parses the specified JSON string into a parse tree
-   * 
+   *
    * @param json JSON text
-   * @return a parse tree of {@link JsonElement}s corresponding to the specified JSON 
+   * @return a parse tree of {@link JsonElement}s corresponding to the specified JSON
    * @throws JsonParseException if the specified text is not valid JSON
    * @since 1.3
    */
   public JsonElement parse(Reader json) throws JsonIOException, JsonSyntaxException {
-    return parse(new JsonReader(json));
+    try {
+      JsonReader jsonReader = new JsonReader(json);
+      JsonElement element = parse(jsonReader);
+      if (!element.isJsonNull() && jsonReader.peek() != JsonToken.END_DOCUMENT) {
+        throw new JsonSyntaxException("Did not consume the entire document.");
+      }
+      return element;
+    } catch (MalformedJsonException e) {
+      throw new JsonSyntaxException(e);
+    } catch (IOException e) {
+      throw new JsonIOException(e);
+    } catch (NumberFormatException e) {
+      throw new JsonSyntaxException(e);
+    }
   }
 
   /**
