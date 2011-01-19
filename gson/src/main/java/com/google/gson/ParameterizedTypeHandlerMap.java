d15c384fdb281e9aba32641e5b7312f32f24fb2d
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2011-01-19 23:28:28 +0000

    Registering default type hierarchy adapters first and allow users to override them. This allows the default EnumTypeAdapter to be overridden for a specific hierachy adapter for Enum with anonymized sub-classes.

--- gson/src/main/java/com/google/gson/ParameterizedTypeHandlerMap.java
+++ gson/src/main/java/com/google/gson/ParameterizedTypeHandlerMap.java
@@ -105,6 +105,21 @@ final class ParameterizedTypeHandlerMap<T> {
     }
   }
 
+  public synchronized void register(ParameterizedTypeHandlerMap<T> other) {
+    if (!modifiable) {
+      throw new IllegalStateException("Attempted to modify an unmodifiable map.");
+    }
+    for (Map.Entry<Type, T> entry : other.map.entrySet()) {
+      register(entry.getKey(), entry.getValue());
+    }
+    // Quite important to traverse the typeHierarchyList from stack bottom first since
+    // we want to register the handlers in the same order to preserve priority order
+    for (int i = other.typeHierarchyList.size()-1; i >= 0; --i) {
+      Pair<Class<?>, T> entry = other.typeHierarchyList.get(i);
+      registerForTypeHierarchy(entry);
+    }
+  }
+
   public synchronized void registerIfAbsent(Type typeOfT, T value) {
     if (!modifiable) {
       throw new IllegalStateException("Attempted to modify an unmodifiable map.");
