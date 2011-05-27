874954126ee917aaa47bbd6f4dfd7d0975bea36f
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2011-05-27 18:33:03 +0000

    Added an example of using raw collection for serialization and deserialization with Gson.

---
+++ trunk/extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.gson.extras.examples.rawcollections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+public class RawCollectionsExample {
+  public static final String json = "['hello',5,{name:'GREETINGS',source:'guest'}]";
+
+  static class Event {
+    private String name;
+    private String source;
+    private Event(String name, String source) {
+      this.name = name;
+      this.source = source;
+    }
+    @Override
+    public String toString() {
+      return String.format("(name=%s, source=%s)", name, source);
+    }
+  }
+  
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) {
+    Gson gson = new Gson();
+    Collection collection = new ArrayList();
+    collection.add("hello");
+    collection.add(5);
+    collection.add(new Event("GREETINGS", "guest"));
+    String json2 = gson.toJson(collection);
+    System.out.println("Using Gson.toJson() on a raw collection: " + json2);
+    JsonParser parser = new JsonParser();
+    JsonArray array = parser.parse(json).getAsJsonArray();
+    String message = gson.fromJson(array.get(0), String.class);
+    int number = gson.fromJson(array.get(1), int.class);
+    Event event = gson.fromJson(array.get(2), Event.class);
+    System.out.printf("Using Gson.fromJson() to get: %s, %d, %s", message, number, event);
+  }
+}
