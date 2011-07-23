a85807818fa5535517ba25353e2875da0878f439
Inderjeet Singh <inder123@gmail.com>
2011-07-23 01:08:34 +0000

    Invoking Gson toJson and fromJson with-in the Android application. Gson.fromJson does not currently work in this application with proguard enabled: The reason seems to be that Cart's field List<LineItem> gets rewritten as raw List type.

--- examples/android-proguard-example/src/com/google/gson/examples/android/GsonProguardExampleActivity.java
+++ examples/android-proguard-example/src/com/google/gson/examples/android/GsonProguardExampleActivity.java
@@ -15,9 +15,16 @@
  */
 package com.google.gson.examples.android;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.examples.android.model.Cart;
+import com.google.gson.examples.android.model.LineItem;
 
 /**
  * Activity class illustrating how to use proguard with Gson
@@ -30,7 +37,24 @@ public class GsonProguardExampleActivity extends Activity {
     super.onCreate(icicle);
     setContentView(R.layout.main);
     TextView tv = (TextView) findViewById(R.id.tv);
-    tv.setText("Hello World");
+    Gson gson = new Gson();
+    Cart cart = buildCart();
+    StringBuilder sb = new StringBuilder();
+    sb.append("Gson.toJson() example: \n");
+    sb.append("  Cart Object: ").append(cart).append("\n");
+    sb.append("  Cart JSON: ").append(gson.toJson(cart)).append("\n");
+    sb.append("\n\nGson.fromJson() example: \n");
+    String json = "{buyer:'Happy Camper',creditCard:'4111-1111-1111-1111',"
+      + "lineItems:[{name:'nails',priceInMicros:100000,quantity:100,currencyCode:'USD'}]}";
+    sb.append("Cart JSON: ").append(json).append("\n");
+    sb.append("Cart Object: ").append(gson.fromJson(json, Cart.class)).append("\n");
+    tv.setText(sb.toString());
     tv.invalidate();
+  }
+
+  private Cart buildCart() {
+    List<LineItem> lineItems = new ArrayList<LineItem>();
+    lineItems.add(new LineItem("hammer", 1, 12000000, "USD"));
+    return new Cart(lineItems, "Happy Buyer", "4111-1111-1111-1111");
   }
 }
---
+++ examples/android-proguard-example/src/com/google/gson/examples/android/model/LineItem.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.gson.examples.android.model;
+
+/**
+ * A line item in a cart. This is not a rest resource, just a dependent object
+ *
+ * @author Inderjeet Singh
+ */
+public class LineItem {
+  private final String name;
+  private final int quantity;
+  private final long priceInMicros;
+  private final String currencyCode;
+
+  public LineItem(String name, int quantity, long priceInMicros, String currencyCode) {
+    this.name = name;
+    this.quantity = quantity;
+    this.priceInMicros = priceInMicros;
+    this.currencyCode = currencyCode;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public long getPriceInMicros() {
+    return priceInMicros;
+  }
+
+  public String getCurrencyCode() {
+    return currencyCode;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("(item: %s, qty: %s, price: %.2f %s)",
+        name, quantity, priceInMicros/(double)1000000, currencyCode);
+  }
+}
