d8818a361c1788603301ef381e5524595d8b5f30
joel.leitch <joel.leitch@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2011-08-12 02:20:48 +0000

    Adding new type adapters for BigInteger and BigDecimal types.

---
+++ trunk/gson/src/main/java/com/google/gson/internal/bind/BigIntegerTypeAdapter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gson.internal.bind;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+/**
+ * Adapts a BigInteger type to and from its JSON representation.
+ *
+ * @author Joel Leitch
+ */
+public class BigIntegerTypeAdapter extends TypeAdapter<BigInteger> {
+
+  @Override
+  public BigInteger read(JsonReader reader) throws IOException {
+    return new BigInteger(reader.nextString());
+  }
+
+  @Override
+  public void write(JsonWriter writer, BigInteger value) throws IOException {
+    writer.value(value);
+  }
+}
