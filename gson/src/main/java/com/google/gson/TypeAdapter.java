fb705976909838f33b02f0d5524cdc2a2d4b35ef
inder123 <inder123@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2011-12-05 19:50:49 +0000

    Added a new API method nullSafe() in TypeAdapter that can be used to avoid boilerplate handling of nulls in a type adapter.

--- gson/src/main/java/com/google/gson/TypeAdapter.java
+++ gson/src/main/java/com/google/gson/TypeAdapter.java
@@ -16,16 +16,18 @@
 
 package com.google.gson;
 
-import com.google.gson.internal.bind.JsonElementWriter;
-import com.google.gson.internal.bind.JsonTreeReader;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
+
+import com.google.gson.internal.bind.JsonElementWriter;
+import com.google.gson.internal.bind.JsonTreeReader;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 
 /**
  * Converts between Java objects and JSON. Applications use type adapters both
@@ -133,6 +135,63 @@ public abstract class TypeAdapter<T> {
   public final void toJson(Writer out, T value) throws IOException {
     JsonWriter writer = new JsonWriter(out);
     write(writer, value);
+  }
+
+  /**
+   * This wrapper method is used to make a type adapter null tolerant. In general, a
+   * type adapter is required to handle nulls in write and read methods. Here is how this
+   * is typically done:<br>
+   * <pre>{@code
+   Gson gson = new GsonBuilder().registerTypeAdapter(Foo.class,
+    new TypeAdapter<Foo>() {
+      public Foo read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+          in.nextNull();
+          return null;
+        }
+        // read a Foo from in and return it
+      }
+      public void write(JsonWriter out, Foo src) throws IOException {
+        if (src == null) {
+          out.nullValue();
+          return;
+        }
+        // write src as JSON to out
+      }
+    ).create();
+   * }</pre>
+   * You can avoid this boilerplate handling of nulls by wrapping your type adapter with
+   * {@link #nullSafe(TypeAdapter)} method. Here is how we will rewrite the above example:
+   * <pre>{@code
+   Gson gson = new GsonBuilder().registerTypeAdapter(Foo.class,
+    TypeAdapter.nullSafe(new TypeAdapter<Foo>() {
+      public Foo read(JsonReader in) throws IOException {
+        // read a Foo from in and return it
+      }
+      public void write(JsonWriter out, Foo src) throws IOException {
+        // write src as JSON to out
+      }
+    )).create();
+   * }</pre>
+   * Note that we didn't need to check for nulls in our type adapter after we used nullSafe. 
+   */
+  public static <T> TypeAdapter<T> nullSafe(final TypeAdapter<T> typeAdapter) {
+    return new TypeAdapter<T>() {
+      @Override public void write(JsonWriter out, T value) throws IOException {
+        if (value == null) {
+          out.nullValue();
+        } else {
+          typeAdapter.write(out, value);
+        }
+      }
+      @Override public T read(JsonReader reader) throws IOException {
+        if (reader.peek() == JsonToken.NULL) {
+          reader.nextNull();
+          return null;
+        }
+        return typeAdapter.read(reader);
+      }
+    };
   }
 
   /**
