f0f02cd341bcf2381fc38f10594b3e478cb6d38d
limpbizkit <limpbizkit@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2011-12-30 07:34:43 +0000

    New extension: handle circular references.
    Serialize graphs of objects by assigning each instance a generated ID and writing the complete graph out as a list. The output for a cycle of Rock/Scissors/Paper looks like this:
    
    {
      '0x1':{'name':'ROCK','beats':'0x2'},
      '0x2':{'name':'SCISSORS','beats':'0x3'},
      '0x3':{'name':'PAPER','beats':'0x1'}
    }
    
    This is work towards issue 137. The hard part is going to be deserializing that back into a graph.

---
+++ trunk/extras/src/test/java/com/google/gson/graph/GraphTypeAdapterFactoryTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gson.graph;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import junit.framework.TestCase;
+
+public final class GraphTypeAdapterFactoryTest extends TestCase {
+  public void testBasicCycle() {
+    Roshambo rock = new Roshambo("ROCK");
+    Roshambo scissors = new Roshambo("SCISSORS");
+    Roshambo paper = new Roshambo("PAPER");
+    rock.beats = scissors;
+    scissors.beats = paper;
+    paper.beats = rock;
+
+    Gson gson = new GsonBuilder()
+        .registerTypeAdapterFactory(GraphTypeAdapterFactory.of(Roshambo.class))
+        .create();
+
+    assertEquals("{'0x1':{'name':'ROCK','beats':'0x2'}," +
+        "'0x2':{'name':'SCISSORS','beats':'0x3'}," +
+        "'0x3':{'name':'PAPER','beats':'0x1'}}",
+        gson.toJson(rock).replace('\"', '\''));
+  }
+
+  static class Roshambo {
+    String name;
+    Roshambo beats;
+    Roshambo(String name) {
+      this.name = name;
+    }
+  }
+}
