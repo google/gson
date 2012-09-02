93f61c5737060b9a8b0090e099da185c278b2bc1
limpbizkit@gmail.com <limpbizkit@gmail.com@2534bb62-2c4b-0410-85e8-b5006b95c4ae>
2012-09-02 20:12:19 +0000

    Change number parsing to use one big loop. This changes it to return JsonToken.STRING for very long (>8k digits) numbers.

--- trunk/gson/src/main/java/com/google/gson/stream/JsonReader.java
+++ trunk/gson/src/main/java/com/google/gson/stream/JsonReader.java
@@ -624,83 +624,116 @@ public class JsonReader implements Closeable {
     return peeked = peeking;
   }
 
+  private static final int NUMBER_CHAR_NONE = 0;
+  private static final int NUMBER_CHAR_SIGN = 1;
+  private static final int NUMBER_CHAR_DIGIT = 2;
+  private static final int NUMBER_CHAR_DECIMAL = 3;
+  private static final int NUMBER_CHAR_FRACTION_DIGIT = 4;
+  private static final int NUMBER_CHAR_EXP_E = 5;
+  private static final int NUMBER_CHAR_EXP_SIGN = 6;
+  private static final int NUMBER_CHAR_EXP_DIGIT = 7;
+
   private int peekNumber() throws IOException {
+    // Like nextNonWhitespace, this uses locals 'p' and 'l' to save inner-loop field access.
+    char[] buffer = this.buffer;
+    int p = pos;
+    int l = limit;
+
     long value = 0; // Negative to accommodate Long.MIN_VALUE more easily.
     boolean negative = false;
     boolean fitsInLong = true;
+    int last = NUMBER_CHAR_NONE;
+
     int i = 0;
-    int c = get(i);
 
-    // TODO: figure out a way to speed up repopulating 'c'
+    charactersOfNumber:
+    for (; true; i++) {
+      if (p + i == l) {
+        if (i == buffer.length) {
+          // Though this looks like a well-formed number, it's too long to continue reading. Give up
+          // and let the application handle this as an unquoted literal.
+          return PEEKED_NONE;
+        }
+        if (!fillBuffer(i + 1)) {
+          break;
+        }
+        p = pos;
+        l = limit;
+      }
 
-    if (c == '-') {
-      negative = true;
-      c = get(++i);
+      char c = buffer[p + i];
+      switch (c) {
+      case '-':
+        if (last == NUMBER_CHAR_NONE) {
+          negative = true;
+          last = NUMBER_CHAR_SIGN;
+          continue;
+        } else if (last == NUMBER_CHAR_EXP_E) {
+          last = NUMBER_CHAR_EXP_SIGN;
+          continue;
+        }
+        return PEEKED_NONE;
+
+      case '+':
+        if (last == NUMBER_CHAR_EXP_E) {
+          last = NUMBER_CHAR_EXP_SIGN;
+          continue;
+        }
+        return PEEKED_NONE;
+
+      case 'e':
+      case 'E':
+        if (last == NUMBER_CHAR_DIGIT || last == NUMBER_CHAR_FRACTION_DIGIT) {
+          last = NUMBER_CHAR_EXP_E;
+          continue;
+        }
+        return PEEKED_NONE;
+
+      case '.':
+        if (last == NUMBER_CHAR_DIGIT) {
+          last = NUMBER_CHAR_DECIMAL;
+          continue;
+        }
+        return PEEKED_NONE;
+
+      default:
+        if (c < '0' || c > '9') {
+          if (!isLiteral(c)) {
+            break charactersOfNumber;
+          }
+          return PEEKED_NONE;
+        }
+        if (last == NUMBER_CHAR_SIGN || last == NUMBER_CHAR_NONE) {
+          value = -(c - '0');
+          last = NUMBER_CHAR_DIGIT;
+        } else if (last == NUMBER_CHAR_DIGIT) {
+          if (value == 0) {
+            return PEEKED_NONE; // Leading '0' prefix is not allowed (since it could be octal).
+          }
+          long newValue = value * 10 - (c - '0');
+          fitsInLong &= value > MIN_INCOMPLETE_INTEGER
+              || (value == MIN_INCOMPLETE_INTEGER && newValue < value);
+          value = newValue;
+        } else if (last == NUMBER_CHAR_DECIMAL) {
+          last = NUMBER_CHAR_FRACTION_DIGIT;
+        } else if (last == NUMBER_CHAR_EXP_E || last == NUMBER_CHAR_EXP_SIGN) {
+          last = NUMBER_CHAR_EXP_DIGIT;
+        }
+      }
     }
 
-    if (c == '0') {
-      c = get(++i);
-    } else if (c >= '1' && c <= '9') {
-      value -= (c - '0');
-      c = get(++i);
-      while (c >= '0' && c <= '9') {
-        long newValue = value * 10 - (c - '0');
-        fitsInLong &= value > MIN_INCOMPLETE_INTEGER
-            || (value == MIN_INCOMPLETE_INTEGER && newValue < value);
-        value = newValue;
-        c = get(++i);
-      }
+    // We've read a complete number. Decide if it's a PEEKED_LONG or a PEEKED_NUMBER.
+    if (last == NUMBER_CHAR_DIGIT && fitsInLong && (value != Long.MIN_VALUE || negative)) {
+      peekedLong = negative ? value : -value;
+      pos += i;
+      return peeked = PEEKED_LONG;
+    } else if (last == NUMBER_CHAR_DIGIT || last == NUMBER_CHAR_FRACTION_DIGIT
+        || last == NUMBER_CHAR_EXP_DIGIT) {
+      peekedNumberLength = i;
+      return peeked = PEEKED_NUMBER;
     } else {
       return PEEKED_NONE;
     }
-
-    if (c == -1 || !isLiteral((char) c)) {
-      if (fitsInLong && (value != Long.MIN_VALUE || negative)) {
-        peekedLong = negative ? value : -value;
-        pos += i;
-        return peeked = PEEKED_LONG;
-      } else {
-        peekedNumberLength = i;
-        return peeked = PEEKED_NUMBER;
-      }
-    }
-
-    if (c == '.') {
-      c = get(++i);
-      while (c >= '0' && c <= '9') {
-        c = get(++i);
-      }
-    }
-
-    if (c == 'e' || c == 'E') {
-      c = get(++i);
-      if (c == '+' || c == '-') {
-        c = get(++i);
-      }
-      if (c >= '0' && c <= '9') {
-        c = get(++i);
-        while (c >= '0' && c <= '9') {
-          c = get(++i);
-        }
-      } else {
-        return PEEKED_NONE;
-      }
-    }
-
-    if (c == -1 || !isLiteral((char) c)) {
-      peekedNumberLength = i;
-      return peeked = PEEKED_NUMBER;
-    }
-
-    return PEEKED_NONE;
-  }
-
-  /**
-   * Returns a character at position {@code pos + offset}, reading additional
-   * bytes into the buffer if necessary.
-   */
-  private int get(int offset) throws IOException {
-    return (pos + offset < limit || fillBuffer(offset + 1)) ? buffer[pos + offset] : -1;
   }
 
   private boolean isLiteral(char c) throws IOException {
