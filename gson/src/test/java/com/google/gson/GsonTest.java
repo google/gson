d57a371d94cf411ba667fb188ecd31474760081d
Ryan Harter <ryanjharter@gmail.com>
2016-04-25 10:26:41 -0500

    Adds getters for config fields.

---
+++ gson/src/test/java/com/google/gson/GsonTest.java
@@ -0,0 +1,40 @@
+package com.google.gson;
+
+import com.google.gson.internal.Excluder;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import junit.framework.TestCase;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Unit tests for {@link Gson}.
+ *
+ * @author Ryan Harter
+ */
+public class GsonTest extends TestCase {
+
+  private static final Excluder CUSTOM_EXCLUDER = Excluder.DEFAULT
+      .excludeFieldsWithoutExposeAnnotation()
+      .disableInnerClassSerialization();
+
+  private static final FieldNamingStrategy CUSTOM_FIELD_NAMING_STRATEGY = new FieldNamingStrategy() {
+    @Override public String translateName(Field f) {
+      return "foo";
+    }
+  };
+
+  public void testOverridesDefaultExcluder() {
+    Gson gson = new Gson(CUSTOM_EXCLUDER, CUSTOM_FIELD_NAMING_STRATEGY,
+        new HashMap<Type, InstanceCreator<?>>(), true, false, true, false,
+        true, true, false, LongSerializationPolicy.DEFAULT,
+        new ArrayList<TypeAdapterFactory>());
+
+    assertEquals(CUSTOM_EXCLUDER, gson.getExcluder());
+    assertEquals(CUSTOM_FIELD_NAMING_STRATEGY, gson.getFieldNamingStrategy());
+    assertEquals(true, gson.serializeNulls());
+    assertEquals(false, gson.htmlSafe());
+  }
+}
