98b3384b63d29181bb11488d0e724000e7afd041
Zac Sweers <pandanomic@gmail.com>
2016-11-19 14:58:01 -0800

    Fix import

--- gson/src/main/java/com/google/gson/GsonBuilder.java
+++ gson/src/main/java/com/google/gson/GsonBuilder.java
@@ -21,6 +21,7 @@ import java.lang.reflect.Type;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
