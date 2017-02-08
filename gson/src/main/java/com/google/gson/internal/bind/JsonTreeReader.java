b06e23d09e75ebf2ca6701cac82f0bb137bb5674
André Rouél <andre.rouel@gmail.com>
2017-02-08 13:34:35 +0100

    Fix ArrayIndexOutOfBoundsException when skipping a value with JsonTreeReader #1013

--- gson/src/main/java/com/google/gson/internal/bind/JsonTreeReader.java
+++ gson/src/main/java/com/google/gson/internal/bind/JsonTreeReader.java
@@ -259,9 +259,13 @@ public final class JsonTreeReader extends JsonReader {
       pathNames[stackSize - 2] = "null";
     } else {
       popStack();
-      pathNames[stackSize - 1] = "null";
+      if (stackSize > 0) {
+        pathNames[stackSize - 1] = "null";
+      }
     }
-    pathIndices[stackSize - 1]++;
+    if (stackSize > 0) {
+      pathIndices[stackSize - 1]++;
+    }
   }
 
   @Override public String toString() {
