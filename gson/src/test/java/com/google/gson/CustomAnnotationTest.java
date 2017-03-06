200825e7e23d59d42f9b59f25a84df323bb94721
fwonce <fwonce@gmail.com>
2017-03-06 09:50:38 +0800

    add license lines to new files

--- gson/src/test/java/com/google/gson/CustomAnnotationTest.java
+++ gson/src/test/java/com/google/gson/CustomAnnotationTest.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.gson;
 
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import junit.framework.TestCase;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
@@ -10,6 +23,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import junit.framework.TestCase;
 
 /**
  * @author Floyd Wan
@@ -64,16 +82,21 @@ public class CustomAnnotationTest extends TestCase {
     Long id3;
   }
 
-  public void testCustomAnnotationMakes() {
+  public void testBrowserFriendlyInSerialization() {
     GsonBuilder gb = new GsonBuilder();
     gb.registerFieldAdapterFactory(BrowserFriendly.class, new BeingBrowserFriendly());
     Gson gson = gb.create();
 
     Person p = new Person();
-    p.id1 = p.id2 = p.id3 = 123456789123456789L;
+    long largeNumber = 123456789123456789L;
+    p.id1 = p.id2 = p.id3 = largeNumber;
     String json = gson.toJson(p);
-    assertTrue(json.contains("\"id1\":\"123456789123456789\""));
-    assertTrue(json.contains("\"id2\":\"123456789123456789\""));
-    assertTrue(json.contains("\"id3\":123456789123456789"));
+    assertTrue(json.contains("\"id1\":" + quoted(largeNumber)));
+    assertTrue(json.contains("\"id2\":" + quoted(largeNumber)));
+    assertTrue(json.contains("\"id3\":" + largeNumber));
+  }
+
+  private String quoted(long number) {
+    return "\"" + number + "\"";
   }
 }
