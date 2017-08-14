76ca73f108eb5cb3b5b247eedd84a774b58dd220
Warren Smith <warrens@uber.com>
2017-08-14 07:36:08 -0700

    Make GsonBuilder.create() factory order idempotent.

--- gson/src/main/java/com/google/gson/GsonBuilder.java
+++ gson/src/main/java/com/google/gson/GsonBuilder.java
@@ -562,8 +562,12 @@ public final class GsonBuilder {
     List<TypeAdapterFactory> factories = new ArrayList<TypeAdapterFactory>(this.factories.size() + this.hierarchyFactories.size() + 3);
     factories.addAll(this.factories);
     Collections.reverse(factories);
-    Collections.reverse(this.hierarchyFactories);
-    factories.addAll(this.hierarchyFactories);
+
+    List<TypeAdapterFactory> hierarchyFactories = new ArrayList<TypeAdapterFactory>(this.hierarchyFactories.size());
+    hierarchyFactories.addAll(this.hierarchyFactories);
+    Collections.reverse(hierarchyFactories);
+    factories.addAll(hierarchyFactories);
+
     addTypeAdaptersForDate(datePattern, dateStyle, timeStyle, factories);
 
     return new Gson(excluder, fieldNamingPolicy, instanceCreators,
