7ddac52748c59228cb86d08d60b417ce737a0563
michaelsavich <savichmichael@gmail.com>
2017-12-26 11:28:19 -0500

    Java 9 module (#1179)
    * Add module-info.java
    
    * Exclude module-info.java by default

---
+++ gson/src/main/java/com.google.gson/module-info.java
@@ -0,0 +1,8 @@
+module com.google.gson {
+	exports com.google.gson;
+	exports com.google.gson.annotations;
+	exports com.google.gson.reflect;
+	exports com.google.gson.stream;
+
+	requires java.sql;
+}
