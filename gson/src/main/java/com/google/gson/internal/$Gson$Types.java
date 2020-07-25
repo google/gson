9faa6c3ddb9e1b8cd126619df75223e1e44736e8
Marcono1234 <Marcono1234@users.noreply.github.com>
2020-07-25 17:15:41 +0200

    Remove WildcardType check in getCollectionElementType
    The returned Type is never a wildcard due to the changes made to getSupertype
    by commit b1fb9ca9a1bea5440bc6a5b506ccf67236b08243.

--- gson/src/main/java/com/google/gson/internal/$Gson$Types.java
+++ gson/src/main/java/com/google/gson/internal/$Gson$Types.java
@@ -307,9 +307,6 @@ public final class $Gson$Types {
   public static Type getCollectionElementType(Type context, Class<?> contextRawType) {
     Type collectionType = getSupertype(context, contextRawType, Collection.class);
 
-    if (collectionType instanceof WildcardType) {
-      collectionType = ((WildcardType)collectionType).getUpperBounds()[0];
-    }
     if (collectionType instanceof ParameterizedType) {
       return ((ParameterizedType) collectionType).getActualTypeArguments()[0];
     }
