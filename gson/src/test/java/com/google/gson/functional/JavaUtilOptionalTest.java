7280e12054dc1b6d51f1a1c051296cd4515c25de
Nadia Wood <nadia@granitewood.com>
2021-10-24 13:22:03 -0700

    Test setup for Optional

---
+++ gson/src/test/java/com/google/gson/functional/JavaUtilOptionalTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gson.functional;
+
+import com.google.gson.Gson;
+
+import java.util.Optional;
+
+import junit.framework.TestCase;
+
+/**
+ * Functional test for Json serialization and deserialization for java.util.Optional
+ */
+public class JavaUtilOptionalTest extends TestCase {
+  private Gson gson;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    gson = new Gson();
+  }
+
+  public void testPlaceholder() {
+    // Nothing here just yet...
+  }
+
+  private static class StringHolder {
+    Optional<String> value = Optional.empty();
+  }
+
+}
