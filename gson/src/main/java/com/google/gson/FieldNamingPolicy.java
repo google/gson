eaf9a0342d69b4cbbfb3644ce42e196453ce164a
yixingz3 <88013471+yixingz3@users.noreply.github.com>
2021-11-28 12:33:22 -0500

    feat: added UPPER_CASE_WITH_UNDERSCORES in FieldNamingPolicy (#2024)

--- gson/src/main/java/com/google/gson/FieldNamingPolicy.java
+++ gson/src/main/java/com/google/gson/FieldNamingPolicy.java
@@ -44,7 +44,7 @@ public enum FieldNamingPolicy implements FieldNamingStrategy {
    * Using this naming policy with Gson will ensure that the first "letter" of the Java
    * field name is capitalized when serialized to its JSON form.
    *
-   * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
    * <ul>
    *   <li>someFieldName ---&gt; SomeFieldName</li>
    *   <li>_someFieldName ---&gt; _SomeFieldName</li>
@@ -61,7 +61,7 @@ public enum FieldNamingPolicy implements FieldNamingStrategy {
    * field name is capitalized when serialized to its JSON form and the words will be
    * separated by a space.
    *
-   * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
    * <ul>
    *   <li>someFieldName ---&gt; Some Field Name</li>
    *   <li>_someFieldName ---&gt; _Some Field Name</li>
@@ -77,9 +77,27 @@ public enum FieldNamingPolicy implements FieldNamingStrategy {
 
   /**
    * Using this naming policy with Gson will modify the Java Field name from its camel cased
+   * form to an upper case field name where each word is separated by an underscore (_).
+   *
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <ul>
+   *   <li>someFieldName ---&gt; SOME_FIELD_NAME</li>
+   *   <li>_someFieldName ---&gt; _SOME_FIELD_NAME</li>
+   *   <li>aStringField ---&gt; A_STRING_FIELD</li>
+   *   <li>aURL ---&gt; A_U_R_L</li>
+   * </ul>
+   */
+  UPPER_CASE_WITH_UNDERSCORES() {
+    @Override public String translateName(Field f) {
+      return separateCamelCase(f.getName(), '_').toUpperCase(Locale.ENGLISH);
+    }
+  },
+
+  /**
+   * Using this naming policy with Gson will modify the Java Field name from its camel cased
    * form to a lower case field name where each word is separated by an underscore (_).
    *
-   * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
    * <ul>
    *   <li>someFieldName ---&gt; some_field_name</li>
    *   <li>_someFieldName ---&gt; _some_field_name</li>
@@ -97,7 +115,7 @@ public enum FieldNamingPolicy implements FieldNamingStrategy {
    * Using this naming policy with Gson will modify the Java Field name from its camel cased
    * form to a lower case field name where each word is separated by a dash (-).
    *
-   * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
    * <ul>
    *   <li>someFieldName ---&gt; some-field-name</li>
    *   <li>_someFieldName ---&gt; _some-field-name</li>
@@ -120,7 +138,7 @@ public enum FieldNamingPolicy implements FieldNamingStrategy {
    * Using this naming policy with Gson will modify the Java Field name from its camel cased
    * form to a lower case field name where each word is separated by a dot (.).
    *
-   * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
+   * <p>Here are a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
    * <ul>
    *   <li>someFieldName ---&gt; some.field.name</li>
    *   <li>_someFieldName ---&gt; _some.field.name</li>
