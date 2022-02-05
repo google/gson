dce0d069d066a72ccec70460a7cc0613c35e500c
Marcono1234 <Marcono1234@users.noreply.github.com>
2022-02-05 22:03:36 +0100

    Improve imports grouping

--- gson/src/test/java/com/google/gson/JsonParserParameterizedTest.java
+++ gson/src/test/java/com/google/gson/JsonParserParameterizedTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.util.Arrays;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
