ef44268baca5ddce1e4e21553eb374a939239e64
Benedictus-Kent Rachmat <benedictuskent.rachmat.etu@univ-lille.fr>
2022-04-04 09:13:57 +0200

    update level.INFO

--- extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
+++ extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
@@ -18,6 +18,7 @@ package com.google.gson.extras.examples.rawcollections;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -47,11 +48,11 @@ public class RawCollectionsExample {
     collection.add(5);
     collection.add(new Event("GREETINGS", "guest"));
     String json = gson.toJson(collection);
-    logger.log("Using Gson.toJson() on a raw collection: " + json);
+    logger.log(Level.INFO, "Using Gson.toJson() on a raw collection: " + json);
     JsonArray array = JsonParser.parseString(json).getAsJsonArray();
     String message = gson.fromJson(array.get(0), String.class);
     int number = gson.fromJson(array.get(1), int.class);
     Event event = gson.fromJson(array.get(2), Event.class);
-    logger.log("Using Gson.fromJson() to get: %s, %d, %s", message, number, event);
+    logger.log(Level.INFO, "Using Gson.fromJson() to get: %s, %d, %s", message, number, event);
   }
 }
