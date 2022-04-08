c43041772ee80a0bc7f00cc3d749096c22bfcf34
Benedictus-Kent Rachmat <benedictuskent.rachmat.etu@univ-lille.fr>
2022-04-08 23:24:23 +0200

    Update GraphAdapterBuilder.java

--- extras/src/main/java/com/google/gson/graph/GraphAdapterBuilder.java
+++ extras/src/main/java/com/google/gson/graph/GraphAdapterBuilder.java
@@ -100,7 +100,7 @@ public final class GraphAdapterBuilder {
           }
 
           Graph graph = graphThreadLocal.get();  
-
+          boolean check = false;
           /*
            * We have one of two cases:
            *  1. We've encountered the first known object in this graph. Write
@@ -111,19 +111,21 @@ public final class GraphAdapterBuilder {
            */
 
           if (graph == null) { 
+            check = true;
             graph = new Graph(new IdentityHashMap<Object, Element<?>>());
           }
 
           @SuppressWarnings("unchecked") // graph.map guarantees consistency between value and T
           Element<T> element = (Element<T>) graph.map.get(value);
           if (element == null) {
-            //graph = putAndAddElement(value, graph, typeAdapter);
-            element = new Element<T>(value, graph.nextName(), typeAdapter, null);
-            graph.map.put(value, element);
-            graph.queue.add(element);
+            putAndAddElement(value, graph, typeAdapter);
+            // element = new Element<T>(value, graph.nextName(), typeAdapter, null);
+            // graph.map.put(value, element);
+            // graph.queue.add(element);
           }
 
-          if (graph.getClass() == Graph.class) {
+          //if (graph.getClass() == Graph.class) {
+            if(check){
             graphThreadLocal.set(graph);
             try {
               out.beginObject();
