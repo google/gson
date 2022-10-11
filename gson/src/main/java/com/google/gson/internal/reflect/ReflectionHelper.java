a0dc7bfdddfe488510edde8d8abb0727743394c4
Ståle Undheim <github@staale.org>
2022-10-11 18:13:49 +0200

    Support Java Records when present in JVM. (#2201)
    * Support Java Records when present in JVM.
    
    Fixes google/gson#1794
    
    Added support in the ReflectionHelper to detect if a class is a record
    on the JVM (via reflection), and if so, we will create a special
    RecordAdapter to deserialize records, using the canoncial constructor.
    
    The ReflectionTypeAdapterFactory had to be refactored a bit to support
    this. The Adapter class inside the factory is now abstract, with
    concrete implementations for normal field reflection and for Records.
    The common code is in the Adapter, with each implementation
    deserializing values into an intermediary object.
    
    For the FieldReflectionAdapter, the intermediary is actually the final
    result, and field access is used to write to fields as before. For the
    RecordAdapter the intermediary is the Object[] to pass to the Record
    constructor.
    
    * Fixed comments from @Marcono1234
    
    Also updated so that we now use the record accessor method to read out
    values from a record, so that direct field access is not necessary.
    
    Also added some tests, that should only execute on Java versions with
    record support, and be ignored for other JVMs
    
    * Fixed additional comments from @Marcono1234
    
    * Made Adapter in ReflectiveTypeAdapterFactory public
    
    Fix comment from @eamonnmcmanus

--- gson/src/main/java/com/google/gson/internal/reflect/ReflectionHelper.java
+++ gson/src/main/java/com/google/gson/internal/reflect/ReflectionHelper.java
@@ -2,26 +2,64 @@ package com.google.gson.internal.reflect;
 
 import com.google.gson.JsonIOException;
 import com.google.gson.internal.GsonBuildConfig;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+
+import java.lang.reflect.*;
 
 public class ReflectionHelper {
-  private ReflectionHelper() { }
+
+  private static final RecordHelper RECORD_HELPER;
+
+  static {
+    RecordHelper instance;
+    try {
+      // Try to construct the RecordSupportedHelper, if this fails, records are not supported on this JVM.
+      instance = new RecordSupportedHelper();
+    } catch (NoSuchMethodException e) {
+      instance = new RecordNotSupportedHelper();
+    }
+    RECORD_HELPER = instance;
+  }
+
+  private ReflectionHelper() {}
 
   /**
-   * Tries making the field accessible, wrapping any thrown exception in a
-   * {@link JsonIOException} with descriptive message.
+   * Tries making the field accessible, wrapping any thrown exception in a {@link JsonIOException}
+   * with descriptive message.
    *
    * @param field field to make accessible
    * @throws JsonIOException if making the field accessible fails
    */
   public static void makeAccessible(Field field) throws JsonIOException {
+    makeAccessible("field '" + field.getDeclaringClass().getName() + "#" + field.getName() + "'", field);
+  }
+
+  /**
+   * Tries making the constructor accessible, wrapping any thrown exception in a {@link JsonIOException}
+   * with descriptive message.
+   *
+   * @param constructor constructor to make accessible
+   * @throws JsonIOException if making the constructor accessible fails
+   */
+  public static void makeAccessible(Constructor<?> constructor) throws JsonIOException {
+    makeAccessible(
+            "constructor " + constructor + " in " + constructor.getDeclaringClass().getName(),
+            constructor
+    );
+  }
+
+  /**
+   * Internal implementation of making an {@link AccessibleObject} accessible.
+   *
+   * @param description describe what we are attempting to make accessible
+   * @param object the object that {@link AccessibleObject#setAccessible(boolean)} should be called on.
+   * @throws JsonIOException if making the object accessible fails
+   */
+  private static void makeAccessible(String description, AccessibleObject object) throws JsonIOException {
     try {
-      field.setAccessible(true);
+      object.setAccessible(true);
     } catch (Exception exception) {
-      throw new JsonIOException("Failed making field '" + field.getDeclaringClass().getName() + "#"
-          + field.getName() + "' accessible; either change its visibility or write a custom "
-          + "TypeAdapter for its declaring type", exception);
+      throw new JsonIOException("Failed making " + description + "' accessible; either change its visibility "
+              + "or write a custom TypeAdapter for its declaring type", exception);
     }
   }
 
@@ -65,10 +103,149 @@ public class ReflectionHelper {
     }
   }
 
-  public static RuntimeException createExceptionForUnexpectedIllegalAccess(IllegalAccessException exception) {
+  /** If records are supported on the JVM, this is equivalent to a call to Class.isRecord() */
+  public static boolean isRecord(Class<?> raw) {
+    return RECORD_HELPER.isRecord(raw);
+  }
+
+  public static String[] getRecordComponentNames(Class<?> raw) {
+    return RECORD_HELPER.getRecordComponentNames(raw);
+  }
+
+  /** Looks up the record accessor method that corresponds to the given record field */
+  public static Method getAccessor(Class<?> raw, Field field) {
+    return RECORD_HELPER.getAccessor(raw, field);
+  }
+
+  public static <T> Constructor<T> getCanonicalRecordConstructor(Class<T> raw) {
+    return RECORD_HELPER.getCanonicalRecordConstructor(raw);
+  }
+
+  public static RuntimeException createExceptionForUnexpectedIllegalAccess(
+      IllegalAccessException exception) {
     throw new RuntimeException("Unexpected IllegalAccessException occurred (Gson " + GsonBuildConfig.VERSION + "). "
         + "Certain ReflectionAccessFilter features require Java >= 9 to work correctly. If you are not using "
         + "ReflectionAccessFilter, report this to the Gson maintainers.",
         exception);
+  }
+
+
+  public static RuntimeException createExceptionForRecordReflectionException(
+          ReflectiveOperationException exception) {
+    throw new RuntimeException("Unexpected ReflectiveOperationException occurred "
+            + "(Gson " + GsonBuildConfig.VERSION + "). "
+            + "To support Java records, reflection is utilized to read out information "
+            + "about records. All these invocations happens after it is established "
+            + "that records exists in the JVM. This exception is unexpected behaviour.",
+            exception);
+  }
+
+  /**
+   * Internal abstraction over reflection when Records are supported.
+   */
+  private abstract static class RecordHelper {
+    abstract boolean isRecord(Class<?> clazz);
+
+    abstract String[] getRecordComponentNames(Class<?> clazz);
+
+    abstract <T> Constructor<T> getCanonicalRecordConstructor(Class<T> raw);
+
+    public abstract Method getAccessor(Class<?> raw, Field field);
+  }
+
+  private static class RecordSupportedHelper extends RecordHelper {
+    private final Method isRecord;
+    private final Method getRecordComponents;
+    private final Method getName;
+    private final Method getType;
+    private final Method getAccessor;
+
+    private RecordSupportedHelper() throws NoSuchMethodException {
+      isRecord = Class.class.getMethod("isRecord");
+      getRecordComponents = Class.class.getMethod("getRecordComponents");
+      Class<?> recordComponentType = getRecordComponents.getReturnType().getComponentType();
+      getName = recordComponentType.getMethod("getName");
+      getType = recordComponentType.getMethod("getType");
+      getAccessor = recordComponentType.getMethod("getAccessor");
+    }
+
+    @Override
+    boolean isRecord(Class<?> raw) {
+      try {
+        return Boolean.class.cast(isRecord.invoke(raw)).booleanValue();
+      } catch (ReflectiveOperationException e) {
+        throw createExceptionForRecordReflectionException(e);
+      }
+    }
+
+    @Override
+    String[] getRecordComponentNames(Class<?> raw) {
+      try {
+        Object[] recordComponents = (Object[]) getRecordComponents.invoke(raw);
+        String[] componentNames = new String[recordComponents.length];
+        for (int i = 0; i < recordComponents.length; i++) {
+          componentNames[i] = (String) getName.invoke(recordComponents[i]);
+        }
+        return componentNames;
+      } catch (ReflectiveOperationException e) {
+        throw createExceptionForRecordReflectionException(e);
+      }
+    }
+
+    @Override
+    public <T> Constructor<T> getCanonicalRecordConstructor(Class<T> raw) {
+      try {
+        Object[] recordComponents = (Object[]) getRecordComponents.invoke(raw);
+        Class<?>[] recordComponentTypes = new Class<?>[recordComponents.length];
+        for (int i = 0; i < recordComponents.length; i++) {
+          recordComponentTypes[i] = (Class<?>) getType.invoke(recordComponents[i]);
+        }
+        // Uses getDeclaredConstructor because implicit constructor has same visibility as record and might
+        // therefore not be public
+        return raw.getDeclaredConstructor(recordComponentTypes);
+      } catch (ReflectiveOperationException e) {
+        throw createExceptionForRecordReflectionException(e);
+      }
+    }
+
+    @Override
+    public Method getAccessor(Class<?> raw, Field field) {
+      try {
+        // Records consists of record components, each with a unique name, a corresponding field and accessor method
+        // with the same name. Ref.: https://docs.oracle.com/javase/specs/jls/se17/html/jls-8.html#jls-8.10.3
+        return raw.getMethod(field.getName());
+      } catch (ReflectiveOperationException e) {
+        throw createExceptionForRecordReflectionException(e);
+      }
+    }
+  }
+
+  /**
+   * Instance used when records are not supported
+   */
+  private static class RecordNotSupportedHelper extends RecordHelper {
+
+    @Override
+    boolean isRecord(Class<?> clazz) {
+      return false;
+    }
+
+    @Override
+    String[] getRecordComponentNames(Class<?> clazz) {
+      throw new UnsupportedOperationException(
+              "Records are not supported on this JVM, this method should not be called");
+    }
+
+    @Override
+    <T> Constructor<T> getCanonicalRecordConstructor(Class<T> raw) {
+      throw new UnsupportedOperationException(
+              "Records are not supported on this JVM, this method should not be called");
+    }
+
+    @Override
+    public Method getAccessor(Class<?> raw, Field field) {
+      throw new UnsupportedOperationException(
+              "Records are not supported on this JVM, this method should not be called");
+    }
   }
 }
---
+++ gson/src/test/java/com/google/gson/internal/bind/ReflectiveTypeAdapterFactoryTest.java
@@ -0,0 +1,83 @@
+package com.google.gson.internal.bind;
+
+import static org.junit.Assert.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.internal.reflect.ReflectionHelperTest;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.nio.file.attribute.GroupPrincipal;
+import java.nio.file.attribute.UserPrincipal;
+import java.security.Principal;
+import org.junit.AssumptionViolatedException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReflectiveTypeAdapterFactoryTest {
+
+  // The class jdk.net.UnixDomainPrincipal is one of the few Record types that are included in the
+  // JDK.
+  // We use this to test serialization and deserialization of Record classes, so we do not need to
+  // have
+  // record support at the language level for these tests. This class was added in JDK 16.
+  Class<?> unixDomainPrincipalClass;
+
+  @Before
+  public void setUp() throws Exception {
+    try {
+      Class.forName("java.lang.Record");
+      unixDomainPrincipalClass = Class.forName("jdk.net.UnixDomainPrincipal");
+    } catch (ClassNotFoundException e) {
+      // Records not supported, ignore
+      throw new AssumptionViolatedException("java.lang.Record not supported");
+    }
+  }
+
+  @Test
+  public void testCustomAdapterForRecords() {
+    Gson gson = new Gson();
+    TypeAdapter<?> recordAdapter = gson.getAdapter(unixDomainPrincipalClass);
+    TypeAdapter<?> defaultReflectionAdapter = gson.getAdapter(UserPrincipal.class);
+    assertNotEquals(recordAdapter.getClass(), defaultReflectionAdapter.getClass());
+  }
+
+  @Test
+  public void testSerializeRecords() throws ReflectiveOperationException {
+    Gson gson =
+        new GsonBuilder()
+            .registerTypeAdapter(UserPrincipal.class, new PrincipalTypeAdapter<>())
+            .registerTypeAdapter(GroupPrincipal.class, new PrincipalTypeAdapter<>())
+            .create();
+
+    UserPrincipal userPrincipal = gson.fromJson("\"user\"", UserPrincipal.class);
+    GroupPrincipal groupPrincipal = gson.fromJson("\"group\"", GroupPrincipal.class);
+    Object recordInstance =
+        unixDomainPrincipalClass
+            .getDeclaredConstructor(UserPrincipal.class, GroupPrincipal.class)
+            .newInstance(userPrincipal, groupPrincipal);
+    String serialized = gson.toJson(recordInstance);
+    Object deserializedRecordInstance = gson.fromJson(serialized, unixDomainPrincipalClass);
+
+    assertEquals(recordInstance, deserializedRecordInstance);
+    assertEquals("{\"user\":\"user\",\"group\":\"group\"}", serialized);
+  }
+
+  private static class PrincipalTypeAdapter<T extends Principal> extends TypeAdapter<T> {
+    @Override
+    public void write(JsonWriter out, T principal) throws IOException {
+      out.value(principal.getName());
+    }
+
+    @Override
+    public T read(JsonReader in) throws IOException {
+      final String name = in.nextString();
+      // This type adapter is only used for Group and User Principal, both of which are implemented by PrincipalImpl.
+      @SuppressWarnings("unchecked")
+      T principal = (T) new ReflectionHelperTest.PrincipalImpl(name);
+      return principal;
+    }
+  }
+}
