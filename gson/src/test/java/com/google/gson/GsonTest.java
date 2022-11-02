db6a20362eff012771bfa33ccd2ae2d8ca0bff94
Mihai Nita <nmihai_2000@yahoo.com>
2022-11-02 07:44:33 -0700

    Restore copyright year, no reason to update

--- gson/src/test/java/com/google/gson/GsonTest.java
+++ gson/src/test/java/com/google/gson/GsonTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Gson Authors
+ * Copyright (C) 2016 The Gson Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
