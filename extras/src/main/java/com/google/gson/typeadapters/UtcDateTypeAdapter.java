fa2dd8c60a42338cbe73c9a2974f8e9bdbc47867
maicolAntali <maicol.antali.ma@gmail.com>
2022-12-12 20:48:01 +0100

    Fix indentation

--- extras/src/main/java/com/google/gson/typeadapters/UtcDateTypeAdapter.java
+++ extras/src/main/java/com/google/gson/typeadapters/UtcDateTypeAdapter.java
@@ -47,14 +47,14 @@ public final class UtcDateTypeAdapter extends TypeAdapter<Date> {
   public Date read(JsonReader in) throws IOException {
     try {
         switch (in.peek()) {
-            case NULL:
-                in.nextNull();
-                return null;
-            default:
-                String date = in.nextString();
-                // Instead of using iso8601Format.parse(value), we use Jackson's date parsing
-                // This is because Android doesn't support XXX because it is JDK 1.6
-                return parse(date, new ParsePosition(0));
+        case NULL:
+            in.nextNull();
+            return null;
+        default:
+            String date = in.nextString();
+            // Instead of using iso8601Format.parse(value), we use Jackson's date parsing
+            // This is because Android doesn't support XXX because it is JDK 1.6
+            return parse(date, new ParsePosition(0));
         }
     } catch (ParseException e) {
       throw new JsonParseException(e);
