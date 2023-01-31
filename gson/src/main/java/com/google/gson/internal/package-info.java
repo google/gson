e1e91f6745f8e96a3f08a57dde8c6f510020a659
Éamonn McManus <eamonn@mcmanus.net>
2023-01-31 12:52:41 -0800

    Add copyright notices to files that were missing them.
    In each case the copyright year is the year in which the file was first added.

--- gson/src/main/java/com/google/gson/internal/package-info.java
+++ gson/src/main/java/com/google/gson/internal/package-info.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Do NOT use any class in this package as they are meant for internal use in Gson.
  * These classes will very likely change incompatibly in future versions. You have been warned.
