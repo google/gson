373525d11cdacf7f0301205b9719236f46261359
maicolAntali <maicol.antali.ma@gmail.com>
2023-02-19 15:01:45 +0100

    Adds `requires` to `module-info.java`

--- gson/src/main/java/module-info.java
+++ gson/src/main/java/module-info.java
@@ -24,6 +24,9 @@ module com.google.gson {
   exports com.google.gson.reflect;
   exports com.google.gson.stream;
 
+  // Dependency on Error Prone Annotations
+  requires com.google.errorprone.annotations;
+
   // Optional dependency on java.sql
   requires static java.sql;
 
