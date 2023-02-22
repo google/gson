92972bfa8699a5a99fc38df5f08bb329418ba7e7
maicolAntali <maicol.antali.ma@gmail.com>
2023-02-22 10:30:52 +0100

    Fixes `MathAbsoluteNegative` warns
    This commit fix all `MathAbsoluteNegative` warns given by ErrorProne in the `gson/src/test.*` path.

--- gson/src/test/java/com/google/gson/internal/LinkedTreeMapTest.java
+++ gson/src/test/java/com/google/gson/internal/LinkedTreeMapTest.java
@@ -176,12 +176,12 @@ public final class LinkedTreeMapTest {
   }
 
   @Test
-  public void testLargeSetOfRandomKeys() throws Exception {
+  public void testLargeSetOfRandomKeys() {
     Random random = new Random(1367593214724L);
     LinkedTreeMap<String, String> map = new LinkedTreeMap<>();
     String[] keys = new String[1000];
     for (int i = 0; i < keys.length; i++) {
-      keys[i] = Integer.toString(Math.abs(random.nextInt()), 36) + "-" + i;
+      keys[i] = Integer.toString(Math.abs(random.nextInt(Integer.MAX_VALUE)), 36) + "-" + i;
       map.put(keys[i], "" + i);
     }
 
