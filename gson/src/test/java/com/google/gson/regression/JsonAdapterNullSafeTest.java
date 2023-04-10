43684817b44db1b4fbc7cd33944ab3754a8b09d6
Fella ABCI <fella@MacBook-Air-de-Fella.local>
2023-04-10 18:57:20 +0200

    adding documentation to the test

--- gson/src/test/java/com/google/gson/regression/JsonAdapterNullSafeTest.java
+++ gson/src/test/java/com/google/gson/regression/JsonAdapterNullSafeTest.java
@@ -24,28 +24,50 @@ import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
 
+/**
+ * Regression test for a bug in Gson's null-safe handling of {@link JsonAdapter}s.
+ * This test checks that serializing and deserializing a Device object with a recursive JsonAdapterFactory
+ * does not result in a StackOverflowError.
+ */
 public class JsonAdapterNullSafeTest {
   private final Gson gson = new Gson();
 
+  /**
+   * Test for Gson's null-safe handling of JsonAdapter serialization with a recursive JsonAdapterFactory.
+   * This test verifies that serializing a Device object does not result in a StackOverflowError.
+   */
   @Test
   public void testNullSafeBugSerialize() {
     Device device = new Device("ec57803e");
     gson.toJson(device);
   }
-
+  
+  /**
+   * Test for Gson's null-safe handling of JsonAdapter deserialization with a recursive JsonAdapterFactory.
+   * This test verifies that deserializing a Device object from JSON does not result in a StackOverflowError.
+   */
   @Test
   public void testNullSafeBugDeserialize() {
     Device device = gson.fromJson("{'id':'ec57803e2'}", Device.class);
     assertThat(device.id).isEqualTo("ec57803e2");
   }
-
+  /**
+   * A class representing a Device object with a recursive JsonAdapterFactory.
+   * This class uses a recursive call in the JsonAdapterFactory to return a null type adapter
+   * when a recursive call is made.
+   */
+  
   @JsonAdapter(Device.JsonAdapterFactory.class)
   private static final class Device {
     String id;
     Device(String id) {
       this.id = id;
     }
-
+    
+    /**
+     * A JsonAdapterFactory for creating a type adapter for a Device object.
+     * This class includes a recursive call to return a null type adapter on a recursive call.
+     */
     static final class JsonAdapterFactory implements TypeAdapterFactory {
       // The recursiveCall in {@link Device.JsonAdapterFactory} is the source of this bug
       // because we use it to return a null type adapter on a recursive call.
