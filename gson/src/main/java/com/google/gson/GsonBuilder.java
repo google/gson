e6cede1eb5f73ec60da6411fc05fd1fc13c6ba44
Marcono1234 <Marcono1234@users.noreply.github.com>
2023-10-03 17:35:55 +0200

    Improve formatting

--- gson/src/main/java/com/google/gson/GsonBuilder.java
+++ gson/src/main/java/com/google/gson/GsonBuilder.java
@@ -675,7 +675,7 @@ public final class GsonBuilder {
         || typeAdapter instanceof InstanceCreator<?>
         || typeAdapter instanceof TypeAdapter<?>);
 
-    if (isTypeObjectOrJsonElement(type)){
+    if (isTypeObjectOrJsonElement(type)) {
       throw new IllegalArgumentException("Cannot override built-in adapter for " + type);
     }
 
@@ -696,8 +696,8 @@ public final class GsonBuilder {
 
   private boolean isTypeObjectOrJsonElement(Type type) {
     return type instanceof Class &&
-            (type == Object.class
-                    || JsonElement.class.isAssignableFrom((Class<?>) type));
+        (type == Object.class
+            || JsonElement.class.isAssignableFrom((Class<?>) type));
   }
 
   /**
