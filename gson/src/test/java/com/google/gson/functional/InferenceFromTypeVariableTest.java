266043a1338367d921bb99472263ae77ab8e6473
Michal Ševčenko <michal.sevcenko@etnetera.cz>
2023-12-07 08:34:36 +0100

    Unit test for type variable inference test.
    See https://github.com/google/gson/issues/2563

---
+++ gson/src/test/java/com/google/gson/functional/InferenceFromTypeVariableTest.java
@@ -0,0 +1,54 @@
+package com.google.gson.functional;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test deserialization of generic wrapper with type bound.
+ * 
+ * @author sevcenko
+ */
+public class InferenceFromTypeVariableTest {
+  private Gson gson;
+
+  @Before
+  public void setUp() throws Exception {
+    gson = new Gson();
+  }
+
+  public static class Foo {
+    private final String text;
+
+    public Foo(String text) {
+      this.text = text;
+    }
+
+    public String getText() {
+      return text;
+    }
+  }
+
+  public static class BarDynamic<T extends Foo> {
+    private final T foo;
+
+    public BarDynamic(T foo) {
+      this.foo = foo;
+    }
+
+    public T getFoo() {
+      return foo;
+    }
+  }
+
+  @Test
+  public void testSubClassSerialization() {
+    BarDynamic<Foo> bar = new BarDynamic<>(new Foo("foo!"));
+    assertThat(gson.toJson(bar)).isEqualTo("{\"foo\":{\"text\":\"foo!\"}}");
+    // without #2563 fix, this would deserialize foo as Object and fails to assign it to foo field
+    BarDynamic<?> deserialized = gson.fromJson(gson.toJson(bar), BarDynamic.class);
+    assertThat(deserialized.getFoo().getText()).isEqualTo("foo!");
+  }
+}
