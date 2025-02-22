1724ced66449c0b43a80e57d65875e6826bb1d9e
Marcono1234 <Marcono1234@users.noreply.github.com>
2025-02-22 01:32:30 +0100

    Fix numeric conversion to Long when serializing numbers as JsonElement tree

--- gson/src/test/java/com/google/gson/functional/JsonTreeTest.java
+++ gson/src/test/java/com/google/gson/functional/JsonTreeTest.java
@@ -19,13 +19,21 @@ package com.google.gson.functional;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.common.TestTypes.BagOfPrimitives;
+import com.google.gson.internal.LazilyParsedNumber;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -88,6 +96,68 @@ public class JsonTreeTest {
     assertThat(jsonElement.has("stringValue")).isFalse();
   }
 
+  @Test
+  public void testToJsonTreeNumbers() {
+    List<Number> numbers =
+        List.of(
+            (byte) 1,
+            (short) 2,
+            3,
+            4L,
+            5f,
+            6d,
+            BigInteger.valueOf(7),
+            new BigDecimal(8),
+            new LazilyParsedNumber("9"));
+
+    Gson gsonSpecialFloats = new GsonBuilder().serializeSpecialFloatingPointValues().create();
+
+    for (Number number : numbers) {
+      JsonElement json = gson.toJsonTree(number);
+      assertIsNumber(json, number);
+
+      json = gsonSpecialFloats.toJsonTree(number);
+      assertIsNumber(json, number);
+    }
+  }
+
+  /**
+   * Tests {@link Gson#toJsonTree(Object)} with {@link AtomicInteger} and {@link AtomicLong}, which
+   * should be serialized as non-atomic (and non-mutable) numbers.
+   */
+  @Test
+  public void testToJsonTreeAtomicNumbers() {
+    JsonElement json = gson.toJsonTree(new AtomicInteger(1));
+    // Current implementation converts int to long, because there is only `JsonWriter#value(long)`
+    assertIsNumber(json, 1L);
+
+    json = gson.toJsonTree(new AtomicLong(1));
+    assertIsNumber(json, 1L);
+  }
+
+  /** Tests numeric conversion when using {@link Gson#toJsonTree(Object, Type)}. */
+  @Test
+  public void testToJsonTreeNumberConversion() {
+    JsonElement json = gson.toJsonTree(1.5f, int.class);
+    assertIsNumber(json, 1);
+
+    json = gson.toJsonTree(500, byte.class);
+    assertIsNumber(json, (byte) -12);
+
+    json = gson.toJsonTree(1, float.class);
+    assertIsNumber(json, 1f);
+
+    json = gson.toJsonTree(1, double.class);
+    assertIsNumber(json, 1d);
+
+    Gson gsonSpecialFloats = new GsonBuilder().serializeSpecialFloatingPointValues().create();
+    json = gsonSpecialFloats.toJsonTree(1, float.class);
+    assertIsNumber(json, 1f);
+
+    json = gsonSpecialFloats.toJsonTree(1, double.class);
+    assertIsNumber(json, 1d);
+  }
+
   private static void assertContains(JsonObject json, JsonPrimitive child) {
     for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
       JsonElement node = entry.getValue();
@@ -98,6 +168,16 @@ public class JsonTreeTest {
       }
     }
     throw new AssertionError("Does not contain " + child);
+  }
+
+  private static void assertIsNumber(JsonElement json, Number expectedNumber) {
+    assertThat(json).isInstanceOf(JsonPrimitive.class);
+    JsonPrimitive jsonPrimitive = (JsonPrimitive) json;
+    assertThat(jsonPrimitive.isNumber()).isTrue();
+    Number number = jsonPrimitive.getAsNumber();
+    assertThat(number).isEqualTo(expectedNumber);
+    // Explicitly check class because Truth's `isEqualTo` is lenient for numeric values
+    assertThat(number.getClass()).isEqualTo(expectedNumber.getClass());
   }
 
   private static class SubTypeOfBagOfPrimitives extends BagOfPrimitives {
